8939eaa72841b1cb53f83825b335d93031c207a5
Truls Jørgensen <truls.jorgensen@nav.no>
2017-11-09 13:39:49 +0100

    a super basic app

---
+++ src/main/java/no/nav/foreldrepenger/front/HelloApusApplication.java
@@ -0,0 +1,12 @@
+package no.nav.foreldrepenger.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  HelloApusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HelloApusApplication.class, args);
+  }
+}
