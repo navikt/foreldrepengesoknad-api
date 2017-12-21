9856b5227d3bb4bb5c36c4936ed19ba9cd8efbe5
Christian H. Mosveen <christian.mosveen@nav.no>
2017-12-21 10:29:24 +0100

    Logging + management configuration

--- src/main/java/no/nav/foreldrepenger/selvbetjening/engangstonad/rest/Oppstart.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/engangstonad/rest/Oppstart.java
@@ -1,16 +1,22 @@
 package no.nav.foreldrepenger.selvbetjening.engangstonad.rest;
 
 import no.nav.foreldrepenger.selvbetjening.engangstonad.rest.json.Oppstartsinfo;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @RestController
 public class Oppstart {
 
+    private static final Logger LOG = getLogger(Oppstart.class);
+
     @RequestMapping(method = {RequestMethod.GET}, value = "/rest/oppstartsinfo")
     public Oppstartsinfo oppstartsinfo() {
+        LOG.info("Henter info om bruker");
         RestTemplate restTemplate = new RestTemplate();
         Oppstartsinfo oppstartsinfo = restTemplate.getForObject("https://foreldrepenger-selvbetjening-oppslag.nais.preprod.local/startup/?fnr=03016536325", Oppstartsinfo.class);
         return oppstartsinfo;
