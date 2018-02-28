b6076f0a6c2e51a912da2c014fee04e95106ddc5
Jan-Olav Eide <janolaveide@gmail.com>
2018-02-28 09:24:23 +0100

    mutability of controller, use same template for all controllers

--- src/main/java/no/nav/foreldrepenger/selvbetjening/rest/EngangsstonadController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/rest/EngangsstonadController.java
@@ -1,18 +1,29 @@
 package no.nav.foreldrepenger.selvbetjening.rest;
 
-import no.nav.foreldrepenger.selvbetjening.consumer.json.EngangsstonadDto;
-import no.nav.foreldrepenger.selvbetjening.rest.json.Engangsstonad;
-import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
 import static java.time.LocalDateTime.now;
 import static org.slf4j.LoggerFactory.getLogger;
 import static org.springframework.http.ResponseEntity.ok;
+
+import java.net.URI;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import no.nav.foreldrepenger.selvbetjening.consumer.json.EngangsstonadDto;
+import no.nav.foreldrepenger.selvbetjening.rest.json.Engangsstonad;
 
 @CrossOrigin
 @RestController
@@ -23,11 +34,18 @@ public class EngangsstonadController {
 
     private static final Logger LOG = getLogger(EngangsstonadController.class);
 
-    @Value("${FPSOKNAD_MOTTAK_API_URL}")
-    private String mottakServiceUrl;
+    private final RestTemplate template;
 
-    @Value("${FORELDREPENGESOKNAD_API_FPSOKNAD_MOTTAK_API_APIKEY_PASSWORD}")
-    private String apiGatewayKey;
+    private final URI mottakServiceUrl;
+
+    private final MeterRegistry registry;
+
+    public EngangsstonadController(RestTemplate template,
+            @Value("${FPSOKNAD_MOTTAK_API_URL}") URI baseURI, MeterRegistry registry) {
+        this.template = template;
+        this.mottakServiceUrl = mottakURIFra(baseURI);
+        this.registry = registry;
+    }
 
     @GetMapping("/{id}")
     public Engangsstonad hentEngangsstonad(@PathVariable String id) {
@@ -44,21 +62,23 @@ public class EngangsstonadController {
         engangsstonad.opprettet = now();
 
         if (stub) {
-            engangsstonad.id = "69";
+            engangsstonad.id = "42";
             return ok(engangsstonad);
         }
-
-        String url = mottakServiceUrl + "/mottak/dokmot/send";
-        LOG.info("Mottak URL: " + url);
-
-        new RestTemplate().postForEntity(url, entity(engangsstonad), String.class);
+        LOG.info("Mottak URL: " + mottakServiceUrl);
+        template.postForEntity(mottakServiceUrl, new HttpEntity<>(new EngangsstonadDto(engangsstonad)), String.class);
         return ok(engangsstonad);
     }
 
-    private HttpEntity<EngangsstonadDto> entity(Engangsstonad engangsstonad) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("x-nav-apiKey", apiGatewayKey);
-        return new HttpEntity<>(new EngangsstonadDto(engangsstonad), headers);
+    private static URI mottakURIFra(URI baseUri) {
+        return UriComponentsBuilder
+                .fromUri(baseUri)
+                .path("/mottak/dokmot/send")
+                .build().toUri();
     }
 
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [template=" + template + ", mottakServiceUrl=" + mottakServiceUrl + "]";
+    }
 }
