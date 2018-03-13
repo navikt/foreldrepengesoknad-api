b6b544a7b1910c632a7710265d694c4f2ede51cd
Christian H. Mosveen <christian.mosveen@nav.no>
2018-03-13 13:10:27 +0100

    Update oppslag URL

--- src/main/java/no/nav/foreldrepenger/selvbetjening/consumer/Oppslagstjeneste.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/consumer/Oppslagstjeneste.java
@@ -7,8 +7,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.inject.Inject;
+import java.net.URI;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static org.springframework.web.util.UriComponentsBuilder.fromUri;
 
 @Service
 public class Oppslagstjeneste {
@@ -16,16 +18,16 @@ public class Oppslagstjeneste {
     private static final Logger LOG = getLogger(Oppslagstjeneste.class);
 
     private final RestTemplate template;
-    private final String oppslagServiceUrl;
+    private final URI oppslagServiceUrl;
 
     @Inject
-    public Oppslagstjeneste(@Value("${FPSOKNAD_OPPSLAG_API_URL}") String uri, RestTemplate template) {
+    public Oppslagstjeneste(@Value("${FPSOKNAD_OPPSLAG_API_URL}") URI uri, RestTemplate template) {
         this.oppslagServiceUrl = uri;
         this.template = template;
     }
 
     public PersonDto hentPerson(String fnr) {
-        String url = oppslagServiceUrl + "/person/?fnr=" + fnr;
+        URI url = fromUri(oppslagServiceUrl).path("/person").queryParam("fnr", fnr).build().toUri();
         LOG.info("Oppslag URL: {}", url);
 
         return template.getForObject(url, PersonDto.class);
