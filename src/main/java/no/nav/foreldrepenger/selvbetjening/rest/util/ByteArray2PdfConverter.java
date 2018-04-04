d89e9cbe0790c9063698d9e833574148ecac2af7
Jan-Olav Eide <janolaveide@gmail.com>
2018-04-04 10:07:51 +0200

    added tika for detection, robustified

--- src/main/java/no/nav/foreldrepenger/selvbetjening/rest/util/ByteArray2PdfConverter.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/rest/util/ByteArray2PdfConverter.java
@@ -1,13 +1,19 @@
 package no.nav.foreldrepenger.selvbetjening.rest.util;
 
+import static org.springframework.http.MediaType.IMAGE_JPEG;
+import static org.springframework.http.MediaType.IMAGE_PNG;
 import static org.springframework.util.StreamUtils.copyToByteArray;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
+import org.apache.tika.Tika;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import com.itextpdf.text.Document;
@@ -16,6 +22,20 @@ import com.itextpdf.text.pdf.PdfWriter;
 
 @Component
 public class ByteArray2PdfConverter {
+
+    private final List<MediaType> mediaTypes;
+
+    public ByteArray2PdfConverter() {
+        this(IMAGE_JPEG, IMAGE_PNG);
+    }
+
+    public ByteArray2PdfConverter(MediaType... mediaTypes) {
+        this(Arrays.asList(mediaTypes));
+    }
+
+    public ByteArray2PdfConverter(List<MediaType> mediaTypes) {
+        this.mediaTypes = mediaTypes;
+    }
 
     private static final Logger LOG = LoggerFactory.getLogger(ByteArray2PdfConverter.class);
 
@@ -28,6 +48,10 @@ public class ByteArray2PdfConverter {
     }
 
     public byte[] convert(byte[] bytes) {
+        return shouldConvert(bytes) ? doConvert(bytes) : bytes;
+    }
+
+    private byte[] doConvert(byte[] bytes) {
         try {
             Document document = new Document();
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -41,7 +65,20 @@ public class ByteArray2PdfConverter {
             LOG.debug("Konverterert byte array av lengde {} til ny av lengde {}", bytes.length, converted.length);
             return converted;
         } catch (Exception e) {
+            LOG.warn("Konvertering feilet");
             throw new IllegalArgumentException(e);
         }
+    }
+
+    private boolean shouldConvert(byte[] bytes) {
+        MediaType mediaType = MediaType.valueOf(new Tika().detect(bytes));
+        boolean shouldConvert = mediaTypes.contains(mediaType);
+        LOG.info("{} convert byte stream of type {} to PDF", shouldConvert ? "Will" : "Will not", mediaType);
+        return shouldConvert;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [mediaTypes=" + mediaTypes + "]";
     }
 }
