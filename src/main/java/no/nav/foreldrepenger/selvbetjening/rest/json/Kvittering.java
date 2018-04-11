0d04830ca125c4782e14bb3f86e05bd21e05df2d
Jan-Olav Eide <janolaveide@gmail.com>
2018-04-11 10:14:08 +0200

    *JsonCreator lagt til

--- src/main/java/no/nav/foreldrepenger/selvbetjening/rest/json/Kvittering.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/rest/json/Kvittering.java
@@ -2,12 +2,17 @@ package no.nav.foreldrepenger.selvbetjening.rest.json;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Kvittering {
     public static final Kvittering STUB = new Kvittering("0", LocalDateTime.now());
     public final LocalDateTime mottattDato;
     public final String referanseId;
 
-    public Kvittering(String referanseId, LocalDateTime mottattDato) {
+    @JsonCreator
+    public Kvittering(@JsonProperty("referanseId") String referanseId,
+            @JsonProperty("mottattDato") LocalDateTime mottattDato) {
         this.referanseId = referanseId;
         this.mottattDato = mottattDato;
     }
