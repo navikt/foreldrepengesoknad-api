58cc638e4ad2cf922c8f1e7a4321098222511d12
Jan-Olav Eide <janolaveide@gmail.com>
2018-04-17 09:36:11 +0200

    duplicate exception handler removed

--- src/main/java/no/nav/foreldrepenger/selvbetjening/rest/APIControllerAdvice.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/rest/APIControllerAdvice.java
@@ -1,9 +1,14 @@
 package no.nav.foreldrepenger.selvbetjening.rest;
 
+import static java.util.stream.Collectors.joining;
 import static org.springframework.http.HttpStatus.PAYLOAD_TOO_LARGE;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -19,6 +24,23 @@ public class APIControllerAdvice extends ResponseEntityExceptionHandler {
     @ResponseBody
     protected ResponseEntity<Object> handleTooLargeAttachments(AttachmentsTooLargeException e, WebRequest request) {
         return handleExceptionInternal(e, null, new HttpHeaders(), PAYLOAD_TOO_LARGE, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return handleExceptionInternal(e, validationResponseBody(e), new HttpHeaders(), UNPROCESSABLE_ENTITY, request);
+    }
+
+    private String validationResponseBody(MethodArgumentNotValidException e) {
+        return e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(this::errorMessage)
+                .collect(joining("\n"));
+    }
+
+    private String errorMessage(FieldError error) {
+        return error.getDefaultMessage() + " (" + error.getField() + ")";
     }
 
 }
