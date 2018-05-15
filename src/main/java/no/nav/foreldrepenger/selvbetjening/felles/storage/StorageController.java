c01185bb9040c7c59eff01f3e23caa4ac357293a
Truls Jørgensen <trulsjor@gmail.com>
2018-05-15 10:22:45 +0200

    Removed encryption at directory level, due to S3 complaining about illegal characters

--- src/main/java/no/nav/foreldrepenger/selvbetjening/felles/storage/StorageController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/felles/storage/StorageController.java
@@ -1,7 +1,6 @@
-package no.nav.foreldrepenger.selvbetjening.felles;
+package no.nav.foreldrepenger.selvbetjening.felles.storage;
 
 import no.nav.foreldrepenger.selvbetjening.felles.crypto.Crypto;
-import no.nav.foreldrepenger.selvbetjening.felles.storage.Storage;
 import no.nav.security.oidc.OIDCConstants;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
 import no.nav.security.oidc.context.OIDCValidationContext;
@@ -12,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
-
 import java.util.Optional;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -38,9 +36,9 @@ public class StorageController {
     @GetMapping
     public ResponseEntity<String> retrieveSøknad() {
         String fnr = fnrFromOIDCToken();
-        String directory = encrypt(fnr, fnr);
-        log.info("Retrieving søknad from directory " + directory);
-        Optional<String> encryptedValue = storage.get(directory, KEY);
+
+        log.info("Retrieving søknad from directory " + fnr);
+        Optional<String> encryptedValue = storage.get(fnr, KEY);
         return encryptedValue
                 .map(ev -> ResponseEntity.ok().body(decrypt(ev, fnr)))
                 .orElse(ResponseEntity.notFound().build());
@@ -49,10 +47,9 @@ public class StorageController {
     @PostMapping(consumes = APPLICATION_JSON_VALUE)
     public ResponseEntity<String> storeSøknad(@RequestBody String søknadJson) {
         String fnr = fnrFromOIDCToken();
-        String directory = encrypt(fnr, fnr);
-        log.info("Writing søknad to directory " + directory);
+        log.info("Writing søknad to directory " + fnr);
         String encryptedValue = encrypt(søknadJson, fnr);
-        storage.put(directory, KEY, encryptedValue);
+        storage.put(fnr, KEY, encryptedValue);
         return ResponseEntity.created(null).build();
     }
 
@@ -62,13 +59,13 @@ public class StorageController {
         return context.getClaims("selvbetjening").getClaimSet().getSubject();
     }
 
-    private String encrypt(String plaintext, String salt) {
-        Crypto crypto = new Crypto(encryptionPassphrase, salt);
+    private String encrypt(String plaintext, String fnr) {
+        Crypto crypto = new Crypto(encryptionPassphrase, fnr);
         return crypto.encrypt(plaintext);
     }
 
-    private String decrypt(String encrypted, String salt) {
-        Crypto crypto = new Crypto(encryptionPassphrase, salt);
+    private String decrypt(String encrypted, String fnr) {
+        Crypto crypto = new Crypto(encryptionPassphrase, fnr);
         return crypto.decrypt(encrypted);
     }
 
