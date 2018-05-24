682a7f28a165f1a84ef44e46c872ff6717a53d07
Odd Einar Hoel <odd.einar.hoel@nav.no>
2018-05-24 13:40:49 +0200

    Deserialize søknad as engangsstonad if type field is not defined.

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsending/json/Søknad.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsending/json/Søknad.java
@@ -9,7 +9,8 @@ import java.time.LocalDateTime;
 import static com.fasterxml.jackson.annotation.JsonTypeInfo.Id.NAME;
 
 
-@JsonTypeInfo(use = NAME, property = "type")
+// TODO remove defaultImpl when both frontend apps send json payload with type field.
+@JsonTypeInfo(use = NAME, property = "type", defaultImpl = Engangsstønad.class)
 @JsonSubTypes({
         @Type(value = Engangsstønad.class, name = "engangsstønad"),
         @Type(value = Foreldrepengesøknad.class, name = "foreldrepengesøknad")
