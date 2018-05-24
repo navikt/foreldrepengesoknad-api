96c3e7f402d3e26ecb3c4338b1e1c5bdad6e4380
Christian H. Mosveen <christian.mosveen@nav.no>
2018-05-24 14:43:37 +0200

    Simplify PersonDto

--- src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/tjeneste/json/PersonDto.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/tjeneste/json/PersonDto.java
@@ -20,21 +20,8 @@ public class PersonDto {
     public CountryCode landKode;
     public Bankkonto bankkonto;
 
-
     public PersonDto() {
         this.bankkonto = new Bankkonto();
-    }
-
-    public PersonDto(String fnr, String aktorId, String fornavn, String mellomnavn, String etternavn, String målform,
-            CountryCode landKode, Bankkonto bankkonto) {
-        this.fnr = fnr;
-        this.aktorId = aktorId;
-        this.fornavn = fornavn;
-        this.mellomnavn = mellomnavn;
-        this.etternavn = etternavn;
-        this.målform = målform;
-        this.landKode = landKode;
-        this.bankkonto = bankkonto;
     }
 
     @Override
@@ -42,20 +29,12 @@ public class PersonDto {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PersonDto personDto = (PersonDto) o;
-        return Objects.equals(fnr, personDto.fnr) &&
-                Objects.equals(aktorId, personDto.aktorId) &&
-                Objects.equals(fornavn, personDto.fornavn) &&
-                Objects.equals(mellomnavn, personDto.mellomnavn) &&
-                Objects.equals(etternavn, personDto.etternavn) &&
-                Objects.equals(kjonn, personDto.kjonn) &&
-                Objects.equals(fodselsdato, personDto.fodselsdato) &&
-                Objects.equals(målform, personDto.målform) &&
-                landKode == personDto.landKode;
+        return Objects.equals(fnr, personDto.fnr);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(fnr, aktorId, fornavn, mellomnavn, etternavn, kjonn, fodselsdato, målform, landKode);
+        return Objects.hash(fnr);
     }
 
     @Override
