7d53a56333941c08198e4f1e3e83025213339d31
Jan-Olav Eide <janolaveide@gmail.com>
2018-06-28 14:46:08 +0200

    log OICDexceptions at INFO  when they occur when heading for a whitelisted URI, at WARN otherwise. A destination URL is whitelisted if any of its pathsegments are contained in a configurable whitelist. Default behaviour is that /person is whitelisted

--- src/main/java/no/nav/foreldrepenger/selvbetjening/felles/error/APIControllerAdvice.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/felles/error/APIControllerAdvice.java
@@ -1,13 +1,24 @@
 package no.nav.foreldrepenger.selvbetjening.felles.error;
 
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.PAYLOAD_TOO_LARGE;
+import static org.springframework.http.HttpStatus.UNAUTHORIZED;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 import java.util.Collections;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.BadRequestException;
+
+import org.assertj.core.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,16 +30,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import no.nav.foreldrepenger.selvbetjening.felles.attachments.exceptions.AttachmentConversionException;
 import no.nav.foreldrepenger.selvbetjening.felles.attachments.exceptions.AttachmentTypeUnsupportedException;
 import no.nav.foreldrepenger.selvbetjening.felles.attachments.exceptions.AttachmentsTooLargeException;
 import no.nav.security.spring.oidc.validation.interceptor.OIDCUnauthorizedException;
 
-import javax.ws.rs.BadRequestException;
-
 @ControllerAdvice
 public class APIControllerAdvice extends ResponseEntityExceptionHandler {
+
+    @Value("${oidcexceptions.path.whitelist:/person}")
+    private String[] oidcExceptionsWhitelist;
 
     private static final Logger LOG = LoggerFactory.getLogger(APIControllerAdvice.class);
 
@@ -48,7 +61,8 @@ public class APIControllerAdvice extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(AttachmentTypeUnsupportedException.class)
     @ResponseBody
-    protected ResponseEntity<Object> handleUnsupportedAttachmentError(AttachmentTypeUnsupportedException e, WebRequest req) {
+    protected ResponseEntity<Object> handleUnsupportedAttachmentError(AttachmentTypeUnsupportedException e,
+            WebRequest req) {
         return logAndHandle(UNPROCESSABLE_ENTITY, e, req);
     }
 
@@ -59,13 +73,15 @@ public class APIControllerAdvice extends ResponseEntityExceptionHandler {
     }
 
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e, HttpHeaders headers, HttpStatus status, WebRequest req) {
-        return logAndHandle(UNPROCESSABLE_ENTITY, e, req, validationErrors(e));
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e,
+            HttpHeaders headers, HttpStatus status, WebRequest req) {
+        return logAndHandle(UNPROCESSABLE_ENTITY, e, req, validationErrors(e), true);
     }
 
     @ExceptionHandler({ OIDCUnauthorizedException.class })
-    public ResponseEntity<Object> handleUnauthorized(OIDCUnauthorizedException e, WebRequest req) {
-        return logAndHandle(UNAUTHORIZED, e, req);
+    public ResponseEntity<Object> handleUnauthorized(OIDCUnauthorizedException e, WebRequest req,
+            HttpServletRequest servletRequest) {
+        return logAndHandle(UNAUTHORIZED, e, req, e.getMessage(), shouldWarn(servletRequest.getRequestURI()));
     }
 
     @ExceptionHandler(BadRequestException.class)
@@ -78,13 +94,33 @@ public class APIControllerAdvice extends ResponseEntityExceptionHandler {
         return logAndHandle(INTERNAL_SERVER_ERROR, e, req);
     }
 
-
     private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req) {
-        return logAndHandle(status, e, req, Collections.singletonList(e.getMessage()));
+        return logAndHandle(status, e, req, true);
     }
 
-    private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, List<String> messages) {
-        LOG.warn("{}", messages, e);
+    private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, boolean shouldWarn) {
+        return logAndHandle(status, e, req, e.getMessage(), shouldWarn);
+    }
+
+    private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, String message,
+            boolean shouldWarn) {
+        return logAndHandle(status, e, req, singletonList(message), shouldWarn);
+
+    }
+
+    private boolean shouldWarn(String requestUri) {
+        return Collections.disjoint(UriComponentsBuilder.fromUriString(requestUri).build()
+                .getPathSegments(), Arrays.asList(oidcExceptionsWhitelist));
+    }
+
+    private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, List<String> messages,
+            boolean shouldWarn) {
+        if (shouldWarn) {
+            LOG.warn("{}", messages, e);
+        }
+        else {
+            LOG.info("{}", messages, e);
+        }
         return handleExceptionInternal(e, new ApiError(status, e, messages), new HttpHeaders(), status, req);
     }
 
