175b8ae9f1f080f175c8147cd84d933e9c88aba1
Christian H. Mosveen <christian.mosveen@nav.no>
2018-07-09 13:15:17 +0200

    Allow freelance without list of close relation jobs

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsending/json/FrilansInformasjon.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsending/json/FrilansInformasjon.java
@@ -1,14 +1,13 @@
 package no.nav.foreldrepenger.selvbetjening.innsending.json;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FrilansInformasjon {
 
     public LocalDate oppstart;
-    public Boolean jobberFremdelesSomFrilans;
     public Boolean driverFosterhjem;
-    public Boolean harJobbetForNærVennEllerFamilieSiste10Mnd;
-    public List<Frilansoppdrag> oppdragForNæreVennerEllerFamilieSiste10Mnd;
+    public List<Frilansoppdrag> oppdragForNæreVennerEllerFamilieSiste10Mnd = new ArrayList<>();
 
 }
