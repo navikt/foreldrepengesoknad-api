385c57e8416893a48693e61193862f11a1cf7e0d
Christian H. Mosveen <christian.mosveen@nav.no>
2018-07-12 13:13:50 +0200

    Pull common info up to Søknad

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsending/json/Foreldrepengesøknad.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsending/json/Foreldrepengesøknad.java
@@ -4,15 +4,6 @@ import java.util.List;
 
 public class Foreldrepengesøknad extends Søknad {
 
-    // TODO: Flytt disse til Søknad
-    public Barn barn;
-    public AnnenForelder annenForelder;
-    public Utenlandsopphold informasjonOmUtenlandsopphold;
-
-    public String situasjon;
-
     public List<UttaksplanPeriode> uttaksplan;
-
-
 
 }
