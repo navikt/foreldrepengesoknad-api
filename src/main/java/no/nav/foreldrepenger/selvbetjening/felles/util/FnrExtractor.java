50056b84f81b587e61ed50975b29bc65f36d3deb
Jan-Olav Eide <janolaveide@gmail.com>
2018-08-01 15:09:05 +0200

    robustify FNRExtractor

--- src/main/java/no/nav/foreldrepenger/selvbetjening/felles/util/FnrExtractor.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/felles/util/FnrExtractor.java
@@ -1,6 +1,9 @@
 package no.nav.foreldrepenger.selvbetjening.felles.util;
 
+import com.nimbusds.jwt.JWTClaimsSet;
+
 import no.nav.security.oidc.OIDCConstants;
+import no.nav.security.oidc.context.OIDCClaims;
 import no.nav.security.oidc.context.OIDCRequestContextHolder;
 import no.nav.security.oidc.context.OIDCValidationContext;
 
@@ -9,7 +12,22 @@ public class FnrExtractor {
     public static String extract(OIDCRequestContextHolder ctxHolder) {
         OIDCValidationContext context = (OIDCValidationContext) ctxHolder
                 .getRequestAttribute(OIDCConstants.OIDC_VALIDATION_CONTEXT);
-        return context.getClaims("selvbetjening").getClaimSet().getSubject();
+        if (context == null) {
+            return "ingen";
+        }
+        OIDCClaims claims = context.getClaims("selvbetjening");
+        if (claims == null) {
+            return "ingen";
+        }
+        JWTClaimsSet claimSet = claims.getClaimSet();
+        if (claimSet == null) {
+            return "ingen";
+        }
+        String fnr = claimSet.getSubject();
+        if (fnr == null || fnr.trim().isEmpty()) {
+            return "ingen";
+        }
+        return fnr;
     }
 
 }
