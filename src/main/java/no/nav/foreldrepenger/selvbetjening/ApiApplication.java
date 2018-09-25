c84b142c7f1a32cb9ea5f0f1fc0ef4e53abe00a9
Jan-Olav Eide <janolaveide@gmail.com>
2018-09-25 13:04:01 +0200

    swaggerify

--- src/main/java/no/nav/foreldrepenger/selvbetjening/ApiApplication.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/ApiApplication.java
@@ -1,14 +1,15 @@
 package no.nav.foreldrepenger.selvbetjening;
 
-import io.prometheus.client.hotspot.DefaultExports;
-import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.prometheus.client.hotspot.DefaultExports;
+import no.nav.security.spring.oidc.validation.api.EnableOIDCTokenValidation;
+
 @SpringBootApplication
 @ComponentScan
-@EnableOIDCTokenValidation(ignore = "org.springframework")
+@EnableOIDCTokenValidation(ignore = { "org.springframework", "springfox.documentation" })
 public class ApiApplication {
 
     public static void main(String[] args) {
