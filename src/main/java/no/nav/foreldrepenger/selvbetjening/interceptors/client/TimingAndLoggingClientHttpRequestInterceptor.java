96a082ddf3bf99c4b57b3e46ded8e605f98d97b1
Jan-Olav Eide <janolaveide@gmail.com>
2018-12-17 11:21:17 +0100

    log error response at WARN in interceptor

--- src/main/java/no/nav/foreldrepenger/selvbetjening/interceptors/client/TimingAndLoggingClientHttpRequestInterceptor.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/interceptors/client/TimingAndLoggingClientHttpRequestInterceptor.java
@@ -1,6 +1,8 @@
 package no.nav.foreldrepenger.selvbetjening.interceptors.client;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.springframework.http.HttpStatus.Series.CLIENT_ERROR;
+import static org.springframework.http.HttpStatus.Series.SERVER_ERROR;
 
 import java.io.IOException;
 
@@ -8,6 +10,7 @@ import org.apache.commons.lang3.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpRequestExecution;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.ClientHttpResponse;
@@ -26,7 +29,18 @@ public class TimingAndLoggingClientHttpRequestInterceptor implements ClientHttpR
         timer.start();
         ClientHttpResponse respons = execution.execute(request, body);
         timer.stop();
-        LOG.info("{} - {}. Dette tok {}ms", request.getMethodValue(), request.getURI(), timer.getTime(MILLISECONDS));
+        if (hasError(respons.getStatusCode())) {
+            LOG.warn("{} - {} - ({}). Dette tok {}ms", request.getMethodValue(), request.getURI(),
+                    respons.getStatusCode(), timer.getTime(MILLISECONDS));
+        }
+        else {
+            LOG.info("{} - {} - ({}). Dette tok {}ms", request.getMethodValue(), request.getURI(),
+                    respons.getStatusCode(), timer.getTime(MILLISECONDS));
+        }
         return respons;
+    }
+
+    protected boolean hasError(HttpStatus code) {
+        return code.series() == CLIENT_ERROR || code.series() == SERVER_ERROR;
     }
 }
