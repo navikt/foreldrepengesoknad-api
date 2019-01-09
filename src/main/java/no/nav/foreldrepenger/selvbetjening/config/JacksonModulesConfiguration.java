8cb26de99b8e05638737d52110d76d02e1e2cf1f
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-09 11:22:56 +0100

    parameter names module already registered

--- src/main/java/no/nav/foreldrepenger/selvbetjening/config/JacksonModulesConfiguration.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/config/JacksonModulesConfiguration.java
@@ -1,15 +1,13 @@
 package no.nav.foreldrepenger.selvbetjening.config;
 
-import static com.fasterxml.jackson.annotation.JsonCreator.Mode.PROPERTIES;
-
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 
-//@Configuration
+@Configuration
 public class JacksonModulesConfiguration {
 
     @Bean
@@ -22,8 +20,4 @@ public class JacksonModulesConfiguration {
         return new Jdk8Module();
     }
 
-    @Bean
-    public Module parameterNamesModule() {
-        return new ParameterNamesModule(PROPERTIES);
-    }
 }
