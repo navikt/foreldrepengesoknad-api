62321f0dc3249f977f7c79f2424febefb04b52df
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-17 15:47:01 +0100

    map inntektsmeldinger

--- src/test/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/InnsynTjenesteStub.java
+++ src/test/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/InnsynTjenesteStub.java
@@ -1,19 +1,21 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn;
 
-import no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.domain.UttaksplanPeriode;
-import no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.saker.Behandling;
-import no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.saker.Sak;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.stereotype.Service;
+import static java.time.LocalDate.now;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 
 import java.net.URI;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-import static java.time.LocalDate.now;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Service;
+
+import no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.domain.UttaksplanPeriode;
+import no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.saker.Behandling;
+import no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.saker.Sak;
 
 @Service
 @ConditionalOnProperty(name = "stub.oppslag", havingValue = "true")
@@ -39,7 +41,7 @@ public class InnsynTjenesteStub implements Innsyn {
         LocalDateTime mottattdato = LocalDateTime.now().minusDays(7);
 
         Behandling behandling = new Behandling(mottattdato, mottattdato.plusHours(2), "AVSLU", "FP",
-                "FORP_FODS", null, null, "4869", "NAV Torrevieja");
+                "FORP_FODS", null, null, "4869", "NAV Torrevieja", Collections.emptyList());
 
         return Arrays.asList(
                 new Sak("SAK", "LA8PV", "UBEH", now().minusYears(1), emptyList()),
