0d585b6f8b8b35e95b778fbb72347d34fab78374
Christian H. Mosveen <christian.mosveen@nav.no>
2019-01-17 11:46:04 +0100

    Remove obsolete toggles

--- src/main/java/no/nav/foreldrepenger/selvbetjening/util/Enabled.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/util/Enabled.java
@@ -6,17 +6,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class Enabled {
 
-    public static boolean ENDRINGSSØKNAD;
-    public static boolean FPSAKSAKER;
     public static boolean TPSBARN;
 
     public Enabled(
-            @Value("${TOGGLES_ENABLE_ENDRINGSSOKNAD:false}") boolean endringssøknad,
-            @Value("${TOGGLES_ENABLE_FPSAKSAKER:false}") boolean fpsakSaker,
             @Value("${TOGGLES_ENABLE_TPSBARN:false}") boolean tpsBarn
     ) {
-        Enabled.ENDRINGSSØKNAD = endringssøknad;
-        Enabled.FPSAKSAKER = fpsakSaker;
         Enabled.TPSBARN = tpsBarn;
     }
 
