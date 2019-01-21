7c3890dccba7486cb96bd8cf00afa35776be8647
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-21 16:20:51 +0100

    log status value

--- src/main/java/no/nav/foreldrepenger/selvbetjening/error/ApiExceptionHandler.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/error/ApiExceptionHandler.java
@@ -107,7 +107,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     private ResponseEntity<Object> logAndHandle(HttpStatus status, Exception e, WebRequest req, HttpHeaders headers,
             Object... messages) {
         ApiError apiError = apiErrorFra(status, e, req, messages);
-        LOG.warn("{} [{}]", status, apiError.getMessages(), e);
+        LOG.warn("{} {} ({})", status, apiError.getMessages(), status.value(), e);
         return handleExceptionInternal(e, apiError, headers, status, req);
     }
 
