e6c65c7a90c4c3ae92edfadb878439ac86347dfe
Christian H. Mosveen <christian.mosveen@nav.no>
2019-01-23 09:28:59 +0100

    Fix missing and erroneous country codes

--- src/main/java/no/nav/foreldrepenger/selvbetjening/util/IkkeNordiskEØSLand.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/util/IkkeNordiskEØSLand.java
@@ -1,32 +1,8 @@
 package no.nav.foreldrepenger.selvbetjening.util;
 
-import static com.neovisionaries.i18n.CountryCode.AT;
-import static com.neovisionaries.i18n.CountryCode.BE;
-import static com.neovisionaries.i18n.CountryCode.BG;
-import static com.neovisionaries.i18n.CountryCode.CH;
-import static com.neovisionaries.i18n.CountryCode.CY;
-import static com.neovisionaries.i18n.CountryCode.CZ;
-import static com.neovisionaries.i18n.CountryCode.EE;
-import static com.neovisionaries.i18n.CountryCode.ES;
-import static com.neovisionaries.i18n.CountryCode.FR;
-import static com.neovisionaries.i18n.CountryCode.GR;
-import static com.neovisionaries.i18n.CountryCode.HR;
-import static com.neovisionaries.i18n.CountryCode.HU;
-import static com.neovisionaries.i18n.CountryCode.IE;
-import static com.neovisionaries.i18n.CountryCode.IT;
-import static com.neovisionaries.i18n.CountryCode.LI;
-import static com.neovisionaries.i18n.CountryCode.LT;
-import static com.neovisionaries.i18n.CountryCode.LU;
-import static com.neovisionaries.i18n.CountryCode.LV;
-import static com.neovisionaries.i18n.CountryCode.MT;
-import static com.neovisionaries.i18n.CountryCode.NL;
-import static com.neovisionaries.i18n.CountryCode.PL;
-import static com.neovisionaries.i18n.CountryCode.PT;
-import static com.neovisionaries.i18n.CountryCode.RO;
-import static com.neovisionaries.i18n.CountryCode.SI;
-import static com.neovisionaries.i18n.CountryCode.UK;
-
 import java.util.Arrays;
+
+import static com.neovisionaries.i18n.CountryCode.*;
 import static java.util.stream.Collectors.toList;
 
 import com.neovisionaries.i18n.CountryCode;
@@ -50,13 +26,14 @@ public enum IkkeNordiskEØSLand {
     NEDERLAND(NL),
     POLEN(PL),
     PORTUGAL(PT),
-    ROMANIA(RO), 
+    ROMANIA(RO),
+    SLOVAKIA(SK),
     SLOVENIA(SI), 
     SPANIA(ES), 
     STORBRITANNIAOGNORDIRLAND(UK), 
     SVEITS(CH),
-    TSJEKKIA(GR),
-    TYSKLAND(CZ),
+    TSJEKKIA(CZ),
+    TYSKLAND(DE),
     UNGARN(HU),
     ØSTERRIKE(AT);
 
