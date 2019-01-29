999d09ac4636c23397b4c7f132863d6f19e6efec
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-29 14:36:43 +0100

    run filters always, but do nothing if unauthenticated

--- src/main/java/no/nav/foreldrepenger/selvbetjening/filters/FilterRegistrationUtil.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/filters/FilterRegistrationUtil.java
@@ -1,5 +1,6 @@
 package no.nav.foreldrepenger.selvbetjening.filters;
 
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 
 import java.util.Arrays;
@@ -7,12 +8,18 @@ import java.util.List;
 
 final class FilterRegistrationUtil {
 
+    private static final String ALWAYS = "/*";
+
     private FilterRegistrationUtil() {
     }
 
     static List<String> urlPatternsFor(String... patterns) {
         return Arrays.stream(patterns)
-                .map(pattern -> pattern + "/*")
+                .map(pattern -> pattern + ALWAYS)
                 .collect(toList());
+    }
+
+    static List<String> always() {
+        return singletonList(ALWAYS);
     }
 }
