11c571a0507499b292d4b454d5d8eb4cd4e0c6e2
Jan-Olav Eide <janolaveide@gmail.com>
2019-01-30 09:48:32 +0100

    default is to throw

--- src/main/java/no/nav/foreldrepenger/selvbetjening/filters/HeadersToMDCFilterRegistrationBean.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/filters/HeadersToMDCFilterRegistrationBean.java
@@ -3,14 +3,18 @@ package no.nav.foreldrepenger.selvbetjening.filters;
 import static no.nav.foreldrepenger.selvbetjening.filters.FilterRegistrationUtil.ALWAYS;
 import static no.nav.foreldrepenger.selvbetjening.filters.FilterRegistrationUtil.urlPatternsFor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HeadersToMDCFilterRegistrationBean extends FilterRegistrationBean<HeadersToMDCFilterBean> {
+    private static final Logger LOG = LoggerFactory.getLogger(HeadersToMDCFilterRegistrationBean.class);
 
     public HeadersToMDCFilterRegistrationBean(HeadersToMDCFilterBean headersFilter) {
         setFilter(headersFilter);
         urlPatternsFor("/rest" + ALWAYS);
+        LOG.info("Registrert filter {}", this);
     }
 }
--- src/main/java/no/nav/foreldrepenger/selvbetjening/filters/IDFilterRegistrationBean.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/filters/IDFilterRegistrationBean.java
@@ -3,14 +3,19 @@ package no.nav.foreldrepenger.selvbetjening.filters;
 import static no.nav.foreldrepenger.selvbetjening.filters.FilterRegistrationUtil.ALWAYS;
 import static no.nav.foreldrepenger.selvbetjening.filters.FilterRegistrationUtil.urlPatternsFor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class IDFilterRegistrationBean extends FilterRegistrationBean<IDToMDCFilterBean> {
+    private static final Logger LOG = LoggerFactory.getLogger(IDFilterRegistrationBean.class);
 
     public IDFilterRegistrationBean(IDToMDCFilterBean idFilter) {
         setFilter(idFilter);
         urlPatternsFor("/rest" + ALWAYS);
+        LOG.info("Registrert filter {}", this);
+
     }
 }
