a08fc4e301db7d334c20e0eafdb26a9e11438686
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-02-15 10:36:32 +0100

    Remove rendundant fields from MedlemskapDto and Utenlandsopphold

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/domain/Utenlandsopphold.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/domain/Utenlandsopphold.java
@@ -3,20 +3,14 @@ package no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.domain;
 import java.util.List;
 
 public class Utenlandsopphold {
-
-    public Boolean iNorgePåHendelsestidspunktet;
-    public Boolean iNorgeNeste12Mnd;
-    public Boolean iNorgeSiste12Mnd;
-
     public List<UtenlandsoppholdPeriode> tidligereOpphold;
     public List<UtenlandsoppholdPeriode> senereOpphold;
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " [iNorgePåHendelsestidspunktet=" + iNorgePåHendelsestidspunktet
-                + ", iNorgeNeste12Mnd="
-                + iNorgeNeste12Mnd + ", iNorgeSiste12Mnd=" + iNorgeSiste12Mnd + ", tidligereOpphold=" + tidligereOpphold
-                + ", senereOpphold=" + senereOpphold + "]";
+        return "Utenlandsopphold{" +
+                "tidligereOpphold=" + tidligereOpphold +
+                ", senereOpphold=" + senereOpphold +
+                '}';
     }
-
 }
