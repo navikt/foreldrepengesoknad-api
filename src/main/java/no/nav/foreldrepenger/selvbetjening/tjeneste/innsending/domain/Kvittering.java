569904f493a21632bf37be15208186da123beaf6
Odd Einar Hoel <odd.einar.hoel@nav.no>
2019-02-20 16:39:01 +0100

    Map pdf to kvittering

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/domain/Kvittering.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/domain/Kvittering.java
@@ -9,22 +9,24 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(NON_EMPTY)
 public class Kvittering {
-    public static final Kvittering STUB = new Kvittering(now(),"deadbeef-69-cafebabe-42", "PÅGÅR", "12345", "67890");
+    public static final Kvittering STUB = new Kvittering(now(),"deadbeef-69-cafebabe-42", "PÅGÅR", "12345", "67890", new byte[0]);
 
     public LocalDateTime mottattDato;
     public String referanseId;
     public String leveranseStatus;
     public String journalId;
     public String saksNr;
+    public byte[] pdf;
 
     @SuppressWarnings("unused")
     public Kvittering() {}
 
-    public Kvittering(LocalDateTime mottattDato, String referanseId, String leveranseStatus, String journalId, String saksNr) {
+    public Kvittering(LocalDateTime mottattDato, String referanseId, String leveranseStatus, String journalId, String saksNr, byte[] pdf) {
         this.mottattDato = mottattDato;
         this.referanseId = referanseId;
         this.leveranseStatus = leveranseStatus;
         this.journalId = journalId;
         this.saksNr = saksNr;
+        this.pdf = pdf;
     }
 }
