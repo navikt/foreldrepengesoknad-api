711530b572354edb7984e87d5f9486b3e28fcad8
Jan-Olav Eide <janolaveide@gmail.com>
2019-04-02 13:18:37 +0200

    tidsperiode -> periode

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/domain/UtenlandsoppholdPeriode.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/domain/UtenlandsoppholdPeriode.java
@@ -3,10 +3,10 @@ package no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.domain;
 public class UtenlandsoppholdPeriode {
 
     public String land;
-    public Tidsperiode tidsperiode;
+    public Tidsperiode periode;
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " [land=" + land + ", tidsperiode=" + tidsperiode + "]";
+        return getClass().getSimpleName() + " [land=" + land + ", tidsperiode=" + periode + "]";
     }
 }
