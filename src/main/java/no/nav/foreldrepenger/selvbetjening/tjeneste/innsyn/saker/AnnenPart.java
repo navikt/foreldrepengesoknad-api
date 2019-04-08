f4739964e4c189b992dcfa88dc914829864ce7c0
Jan-Olav Eide <janolaveide@gmail.com>
2019-04-08 12:19:50 +0200

    jacksonify

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/saker/AnnenPart.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/saker/AnnenPart.java
@@ -1,7 +1,35 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.saker;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class AnnenPart {
-    public String fnr;
-    public String aktørId;
-    public Navn navn;
+    private final String fnr;
+    private final String aktørId;
+    private final Navn navn;
+
+    @JsonCreator
+    public AnnenPart(@JsonProperty("fnr") String fnr, @JsonProperty("aktørId") String aktørId,
+            @JsonProperty("navn") Navn navn) {
+        this.fnr = fnr;
+        this.aktørId = aktørId;
+        this.navn = navn;
+    }
+
+    public String getFnr() {
+        return fnr;
+    }
+
+    public String getAktørId() {
+        return aktørId;
+    }
+
+    public Navn getNavn() {
+        return navn;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [fnr=" + fnr + ", aktørId=" + aktørId + ", navn=" + navn + "]";
+    }
 }
--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/saker/Navn.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/saker/Navn.java
@@ -1,9 +1,37 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.saker;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class Navn {
 
-    public String fornavn;
-    public String mellomnavn;
-    public String etternavn;
+    private final String fornavn;
+    private final String mellomnavn;
+    private final String etternavn;
 
+    @JsonCreator
+    public Navn(@JsonProperty("fornavn") String fornavn, @JsonProperty("mellomnavn") String mellomnavn,
+            @JsonProperty("etternavn") String etternavn) {
+        this.fornavn = fornavn;
+        this.mellomnavn = mellomnavn;
+        this.etternavn = etternavn;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public String getMellomnavn() {
+        return mellomnavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + " [fornavn=" + fornavn + ", mellomnavn=" + mellomnavn + ", etternavn="
+                + etternavn + "]";
+    }
 }
