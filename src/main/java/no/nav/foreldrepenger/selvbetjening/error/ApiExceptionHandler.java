c2be0279dcd15527edf1f2b837d68973fce56b5b
Jan-Olav Eide <janolaveide@gmail.com>
2019-04-24 09:01:17 +0200

    added bad request handler so we log it

--- src/main/java/no/nav/foreldrepenger/selvbetjening/error/ApiExceptionHandler.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/error/ApiExceptionHandler.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
@@ -42,7 +41,6 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     private static final Logger LOG = LoggerFactory.getLogger(ApiExceptionHandler.class);
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<Object> handleBadRequest(HttpMessageNotReadableException e, WebRequest request) {
         return logAndHandle(HttpStatus.BAD_REQUEST, e, request);
     }
