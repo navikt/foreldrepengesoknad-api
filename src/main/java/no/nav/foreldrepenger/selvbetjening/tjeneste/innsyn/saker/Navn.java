57dc1d83b9133df7eb414ce5ddbc2310d7ee98fa
Jan-Olav Eide <janolaveide@gmail.com>
2019-05-13 14:04:07 +0200

    Navn har nå også kjønn som attributt

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/saker/Navn.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/saker/Navn.java
@@ -4,17 +4,22 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class Navn {
-
     private final String fornavn;
     private final String mellomnavn;
     private final String etternavn;
+    private final Kjønn kjønn;
 
     @JsonCreator
     public Navn(@JsonProperty("fornavn") String fornavn, @JsonProperty("mellomnavn") String mellomnavn,
-            @JsonProperty("etternavn") String etternavn) {
+            @JsonProperty("etternavn") String etternavn, @JsonProperty("kjønn") Kjønn kjønn) {
         this.fornavn = fornavn;
         this.mellomnavn = mellomnavn;
         this.etternavn = etternavn;
+        this.kjønn = kjønn;
+    }
+
+    public Kjønn getKjønn() {
+        return kjønn;
     }
 
     public String getFornavn() {
@@ -31,7 +36,8 @@ public class Navn {
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " [fornavn=" + fornavn + ", mellomnavn=" + mellomnavn + ", etternavn="
-                + etternavn + "]";
+        return getClass().getSimpleName() + "[fornavn=" + fornavn + ", mellomnavn=" + mellomnavn + ", etternavn="
+                + etternavn + ", kjønn="
+                + kjønn + "]";
     }
 }
