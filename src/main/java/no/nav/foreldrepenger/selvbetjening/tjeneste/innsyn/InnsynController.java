40eb3b2183533c3f58a0ec958bc687f1096f99b4
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-06-06 13:51:25 +0200

    separate endpoint

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/InnsynController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/InnsynController.java
@@ -6,13 +6,15 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import no.nav.foreldrepenger.selvbetjening.error.UnexpectedInputException;
 import no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.saker.Sak;
 import no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.uttaksplan.Uttaksplan;
 import no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.vedtak.Vedtak;
@@ -22,6 +24,8 @@ import no.nav.security.oidc.api.ProtectedWithClaims;
 @RequestMapping(path = InnsynController.INNSYN, produces = APPLICATION_JSON_VALUE)
 @ProtectedWithClaims(issuer = ISSUER, claimMap = { "acr=Level4" })
 public class InnsynController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(InnsynController.class);
 
     public static final String INNSYN = "/rest/innsyn";
 
@@ -38,16 +42,14 @@ public class InnsynController {
     }
 
     @GetMapping("/uttaksplan")
-    public Uttaksplan uttaksplan(@RequestParam(name = "saksnummer") String saksnummer,
-            @RequestParam(name = "annenPart") String annenPart) {
+    public Uttaksplan uttaksplan(@RequestParam(name = "saksnummer") String saksnummer) {
+        return innsynTjeneste.hentUttaksplan(saksnummer);
+    }
 
-        if (saksnummer != null) {
-            return innsynTjeneste.hentUttaksplan(saksnummer);
-        }
-        if (annenPart != null) {
-            return innsynTjeneste.hentUttaksplanAnnenPart(annenPart);
-        }
-        throw new UnexpectedInputException("En av saksnummer og annenPart må være satt (men ikke begge)");
+    @GetMapping("/uttaksplanannen")
+    public Uttaksplan uttaksplanAnnenPart(@RequestParam(name = "annenPart") @NotNull String annenPart) {
+        LOG.info("Henter uttaksplan annen part {}", annenPart);
+        return innsynTjeneste.hentUttaksplanAnnenPart(annenPart);
     }
 
     @GetMapping("/vedtak")
