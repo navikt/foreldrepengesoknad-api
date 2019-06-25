7923d13c6651e20cd25fb4eaa550d8b622ba5d8c
Steffen Pøhner <steffen.h.pohner@nav.no>
2019-06-25 15:48:06 +0200

    Simpler MorsAktivitet-enum

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/vedtak/uttak/MorsAktivitet.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/vedtak/uttak/MorsAktivitet.java
@@ -1,21 +1,7 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.vedtak.uttak;
 
 public enum MorsAktivitet {
-    ARBEID, UTDANNING, KVALPROG("morsaktivitet.kvalprog"), INTROPROG("morsaktivitet.introprog"),
-    TRENGER_HJELP("morsaktivitet.sykdom"), INNLAGT, ARBEID_OG_UTDANNING,
-    SAMTIDIGUTTAK("morsaktivitet.samtidig"), UFØRE;
-
-    private final String key;
-
-    public String getKey() {
-        return key;
-    }
-
-    MorsAktivitet() {
-        this(null);
-    }
-
-    MorsAktivitet(String key) {
-        this.key = key;
-    }
+    ARBEID, UTDANNING, KVALPROG, INTROPROG,
+    TRENGER_HJELP, INNLAGT, ARBEID_OG_UTDANNING,
+    SAMTIDIGUTTAK, UFØRE;
 }
