441b380d1f9ce5c0e9c9f8be1173732b69e60f0b
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-08-15 20:03:22 +0200

    determine spring profile from cluster

---
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/config/ClusterAwareSpringProfileResolver.java
@@ -0,0 +1,24 @@
+package no.nav.foreldrepenger.selvbetjening.config;
+
+import static no.nav.foreldrepenger.historikk.util.EnvUtil.DEFAULT;
+import static no.nav.foreldrepenger.historikk.util.EnvUtil.DEV;
+import static no.nav.foreldrepenger.historikk.util.EnvUtil.LOCAL;
+
+public class ClusterAwareSpringProfileResolver {
+
+    private static final String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
+
+    public String getProfile() {
+        return clusterFra(System.getenv(NAIS_CLUSTER_NAME));
+    }
+
+    private static String clusterFra(String cluster) {
+        if (cluster == null) {
+            return LOCAL;
+        }
+        if (cluster.contains(DEV)) {
+            return DEV;
+        }
+        return DEFAULT;
+    }
+}
