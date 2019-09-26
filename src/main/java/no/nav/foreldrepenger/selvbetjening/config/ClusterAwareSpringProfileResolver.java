abcd8eb7df36a8012ba665771953c200ba8483c1
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-09-26 09:52:40 +0200

    cleanup

--- src/main/java/no/nav/foreldrepenger/selvbetjening/config/ClusterAwareSpringProfileResolver.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/config/ClusterAwareSpringProfileResolver.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.selvbetjening.config;
 
 import static java.lang.System.getenv;
+import static no.nav.foreldrepenger.selvbetjening.util.Constants.NAIS_CLUSTER_NAME;
 import static no.nav.foreldrepenger.selvbetjening.util.EnvUtil.DEFAULT;
 import static no.nav.foreldrepenger.selvbetjening.util.EnvUtil.DEV;
 import static no.nav.foreldrepenger.selvbetjening.util.EnvUtil.LOCAL;
@@ -8,8 +9,6 @@ import static no.nav.foreldrepenger.selvbetjening.util.EnvUtil.LOCAL;
 import java.util.Optional;
 
 public class ClusterAwareSpringProfileResolver {
-
-    private static final String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
 
     private ClusterAwareSpringProfileResolver() {
 
--- src/main/java/no/nav/foreldrepenger/selvbetjening/util/Constants.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/util/Constants.java
@@ -1,6 +1,7 @@
 package no.nav.foreldrepenger.selvbetjening.util;
 
 public final class Constants {
+    public static final String NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME";
     public static final String FNR_HEADER_VALUE = "fnr";
     public static final String NAV_CONSUMER_ID = "Nav-Consumer-Id";
     public static final String NAV_CALL_ID = "Nav-CallId";
