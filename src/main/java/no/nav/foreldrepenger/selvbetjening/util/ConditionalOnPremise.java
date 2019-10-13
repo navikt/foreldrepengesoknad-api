59226c5a4bb3790f55995de23c74ee5bca4665d9
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-10-13 15:26:54 +0200

    test if this works

--- src/main/java/no/nav/foreldrepenger/selvbetjening/util/ConditionalOnPremise.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/util/ConditionalOnPremise.java
@@ -1,9 +1,9 @@
 package no.nav.foreldrepenger.selvbetjening.util;
 
-import static no.nav.foreldrepenger.selvbetjening.util.Cluster.DEV;
-import static no.nav.foreldrepenger.selvbetjening.util.Cluster.PROD;
+import static no.nav.foreldrepenger.selvbetjening.util.Cluster.DEV_SBS;
+import static no.nav.foreldrepenger.selvbetjening.util.Cluster.PROD_SBS;
 
-@ConditionalOnClusters(clusters = { DEV, PROD })
+@ConditionalOnClusters(clusters = { DEV_SBS, PROD_SBS })
 public @interface ConditionalOnPremise {
 
 }
