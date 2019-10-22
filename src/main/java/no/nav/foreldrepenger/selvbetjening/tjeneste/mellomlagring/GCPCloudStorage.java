5e582f6d2133a3735174e91cfbe3a04747e7b388
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-10-22 11:10:26 +0200

    make bucket names configurable, implement ping

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/mellomlagring/GCPCloudStorage.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/mellomlagring/GCPCloudStorage.java
@@ -1,12 +1,15 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.mellomlagring;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8_VALUE;
+
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
@@ -15,50 +18,54 @@ import com.google.cloud.storage.StorageOptions;
 
 public class GCPCloudStorage implements Storage {
 
-    private static final String BUCKET_FORELDREPENGER = "foreldrepengesoknad";
-    private static final String BUCKET_FORELDREPENGER_MELLOMLAGRING = "mellomlagring";
-
     private static final Logger LOG = LoggerFactory.getLogger(GCPCloudStorage.class);
 
     private final com.google.cloud.storage.Storage storage;
+    private final String søknadBucket;
+    private final String mellomlagringBucket;
 
-    public GCPCloudStorage() {
+    public GCPCloudStorage(@Value("${bucket.søknad:foreldrepengesoknad}") String søknadBucket,
+            @Value("${bucket.mellomlagring:mellomlagring}") String mellomlagringBucket) {
         storage = StorageOptions.getDefaultInstance().getService();
+        this.søknadBucket = søknadBucket;
+        this.mellomlagringBucket = mellomlagringBucket;
     }
 
     @Override
     public String ping() {
+        put("ping", "pingKey", "42");
+        delete("ping", "pingKey");
         return "OK";
     }
 
     @Override
     public URI pingURI() {
-        return URI.create("http://localhost");
+        return URI.create("https://storage.googleapis.com");
     }
 
     @Override
     public void put(String directory, String key, String value) {
-        writeString(BUCKET_FORELDREPENGER, directory, key, value);
+        writeString(søknadBucket, directory, key, value);
     }
 
     @Override
     public void putTmp(String directory, String key, String value) {
-        writeString(BUCKET_FORELDREPENGER_MELLOMLAGRING, directory, key, value);
+        writeString(mellomlagringBucket, directory, key, value);
     }
 
     @Override
     public Optional<String> get(String directory, String key) {
-        return Optional.ofNullable(readString(BUCKET_FORELDREPENGER, directory, key));
+        return Optional.ofNullable(readString(søknadBucket, directory, key));
     }
 
     @Override
     public Optional<String> getTmp(String directory, String key) {
-        return Optional.ofNullable(readString(BUCKET_FORELDREPENGER_MELLOMLAGRING, directory, key));
+        return Optional.ofNullable(readString(mellomlagringBucket, directory, key));
     }
 
     @Override
     public void delete(String directory, String key) {
-        deleteString(BUCKET_FORELDREPENGER, directory, key);
+        deleteString(søknadBucket, directory, key);
     }
 
     private void deleteString(String bucketName, String directory, String key) {
@@ -69,15 +76,15 @@ public class GCPCloudStorage implements Storage {
 
     @Override
     public void deleteTmp(String directory, String key) {
-        deleteString(BUCKET_FORELDREPENGER_MELLOMLAGRING, directory, key);
+        deleteString(mellomlagringBucket, directory, key);
     }
 
     private void writeString(String bucketName, String directory, String key, String value) {
         LOG.info("Lagrer objekt i bøtte {}, katalog {}", bucketName, directory);
         Blob blob = storage.create(
                 BlobInfo.newBuilder(BlobId.of(bucketName, fileName(directory, key)))
-                        .setContentType(MediaType.APPLICATION_JSON_UTF8_VALUE).build(),
-                value.getBytes(StandardCharsets.UTF_8));
+                        .setContentType(APPLICATION_JSON_UTF8_VALUE).build(),
+                value.getBytes(UTF_8));
         LOG.info("Lagret objekt {} i bøtte {}", blob, bucketName);
     }
 
@@ -100,7 +107,7 @@ public class GCPCloudStorage implements Storage {
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + "[storage=" + storage + "]";
+        return getClass().getSimpleName() + "[storage=" + storage + ", søknadBucket=" + søknadBucket
+                + ", mellomlagringBucket=" + mellomlagringBucket + "]";
     }
-
 }
