b1fd066352e750694e0530f526bd2cc1278f0601
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-10-30 13:30:41 +0100

    logging

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/mellomlagring/MellomlagringHealthIndicator.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/mellomlagring/MellomlagringHealthIndicator.java
@@ -1,14 +1,12 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.mellomlagring;
 
-import static org.springframework.boot.cloud.CloudPlatform.KUBERNETES;
-
-import org.springframework.boot.autoconfigure.condition.ConditionalOnCloudPlatform;
 import org.springframework.stereotype.Component;
 
 import no.nav.foreldrepenger.selvbetjening.health.AbstractPingableHealthIndicator;
+import no.nav.foreldrepenger.selvbetjening.util.ConditionalOnK8s;
 
 @Component
-@ConditionalOnCloudPlatform(KUBERNETES)
+@ConditionalOnK8s
 public class MellomlagringHealthIndicator extends AbstractPingableHealthIndicator {
 
     public MellomlagringHealthIndicator(Storage pingable) {
