ad6a16b63e75dba61ecc1892e0d0fd7eeb162aea
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-11-01 14:23:01 +0100

    json

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/oppslag/domain/AnnenForelder.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/oppslag/domain/AnnenForelder.java
@@ -4,23 +4,49 @@ import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(NON_NULL)
 public class AnnenForelder {
 
-    public final String fnr;
-    public final String fornavn;
-    public final String mellomnavn;
-    public final String etternavn;
-    public final LocalDate fødselsdato;
+    private final String fnr;
+    private final String fornavn;
+    private final String mellomnavn;
+    private final String etternavn;
+    private final LocalDate fødselsdato;
 
-    public AnnenForelder(String fnr, String fornavn, String mellomnavn, String etternavn, LocalDate fødselsdato) {
+    @JsonCreator
+    public AnnenForelder(@JsonProperty("fnr") String fnr, @JsonProperty("fornavn") String fornavn,
+            @JsonProperty("mellomnavn") String mellomnavn,
+            @JsonProperty("etternavn") String etternavn,
+            @JsonProperty("fødselsdato") LocalDate fødselsdato) {
         this.fnr = fnr;
         this.fornavn = fornavn;
         this.mellomnavn = mellomnavn;
         this.etternavn = etternavn;
         this.fødselsdato = fødselsdato;
+    }
+
+    public String getFnr() {
+        return fnr;
+    }
+
+    public String getFornavn() {
+        return fornavn;
+    }
+
+    public String getMellomnavn() {
+        return mellomnavn;
+    }
+
+    public String getEtternavn() {
+        return etternavn;
+    }
+
+    public LocalDate getFødselsdato() {
+        return fødselsdato;
     }
 
     @Override
