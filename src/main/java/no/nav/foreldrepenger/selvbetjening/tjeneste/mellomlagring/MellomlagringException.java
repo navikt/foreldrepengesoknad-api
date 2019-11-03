bf6534c7d73e13b81000cf0cd49f17aab8e65e63
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-11-03 10:09:57 +0100

    cleanup

---
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/mellomlagring/MellomlagringException.java
@@ -0,0 +1,8 @@
+package no.nav.foreldrepenger.selvbetjening.tjeneste.mellomlagring;
+
+public class MellomlagringException extends RuntimeException {
+    public MellomlagringException(Throwable cause) {
+        super(cause);
+    }
+
+}
