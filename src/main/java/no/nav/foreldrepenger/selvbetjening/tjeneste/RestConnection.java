cabbec054fbaebde73edfc3eb63b389681de0909
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2019-11-13 22:51:11 +0100

    retry on IO errors

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/RestConnection.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/RestConnection.java
@@ -6,8 +6,10 @@ import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.web.client.HttpServerErrorException.BadGateway;
 import org.springframework.web.client.HttpServerErrorException.InternalServerError;
+import org.springframework.web.client.ResourceAccessException;
 
-@Retryable(include = BadGateway.class, exclude = InternalServerError.class, maxAttemptsExpression = "#{${rest.retry.attempts:3}}", backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
+@Retryable(include = { ResourceAccessException.class,
+        BadGateway.class }, exclude = InternalServerError.class, maxAttemptsExpression = "#{${rest.retry.attempts:3}}", backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
 public interface RestConnection extends PingEndpointAware, Togglable {
 
     <T> T getForObject(URI uri, Class<T> responseType);
