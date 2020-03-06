e810709944a3b5f7296656fe62b2da44b9bace23
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2020-03-06 10:34:47 +0100

    Revert "TFP-2510 Use pdf generator app for TilbakekrevingUttalelse"

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/pdf/PDFGenerator.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/pdf/PDFGenerator.java
@@ -1,22 +1,22 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.pdf;
 
-import no.nav.foreldrepenger.selvbetjening.error.UnexpectedInputException;
-import no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.domain.tilbakebetaling.TilbakebetalingUttalelse;
+import java.io.ByteArrayOutputStream;
+
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
-import java.io.ByteArrayOutputStream;
+import no.nav.foreldrepenger.selvbetjening.error.UnexpectedInputException;
 
-public class PdfGeneratorStub implements PdfGenerator {
+@Component
+public class PDFGenerator {
 
     public byte[] generate(String overskrift, String tekst) {
 
         try (PDDocument document = new PDDocument();
-             ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
 
             PDPage page = new PDPage();
             document.addPage(page);
@@ -34,11 +34,4 @@ public class PdfGeneratorStub implements PdfGenerator {
             throw new UnexpectedInputException("Kunne ikke lage PDF", e);
         }
     }
-
-    @Override
-    public byte[] generate(TilbakebetalingUttalelse uttalelse) {
-        return generate("Stubbet test", uttalelse.getBrukerTekst().getTekst());
-    }
 }
-
-
--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/pdf/PdfGenerator.java
+++
@@ -1,9 +0,0 @@
-package no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.pdf;
-
-import no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.domain.tilbakebetaling.TilbakebetalingUttalelse;
-
-public interface PdfGenerator {
-
-    byte[] generate(TilbakebetalingUttalelse uttalelse);
-
-}
