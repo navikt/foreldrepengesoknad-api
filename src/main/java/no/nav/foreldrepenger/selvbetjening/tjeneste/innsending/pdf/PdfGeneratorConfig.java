331358f9d0fdbcacf5a138751a73170c1355bb83
Steffen Holst Pøhner <steffen.h.pohner@nav.no>
2020-03-06 13:24:47 +0100

    TFP-2510 Use pdf generator app for TilbakekrevingUttalelse

---
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/pdf/PdfGeneratorConfig.java
@@ -0,0 +1,27 @@
+package no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.pdf;
+
+import no.nav.foreldrepenger.selvbetjening.tjeneste.AbstractConfig;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import java.net.URI;
+
+@ConfigurationProperties(prefix = "pdfgenerator")
+public class PdfGeneratorConfig extends AbstractConfig {
+
+    private static final String TILBAKEBETALING_UTTALELSE = "tilbakebetaling/uttalelse";
+    private static final String PING = "/is_alive";
+
+    public PdfGeneratorConfig(URI uri, @DefaultValue("true") boolean enabled) {
+        super(uri, enabled);
+    }
+
+    @Override
+    protected URI pingURI() {
+        return uri(getUri(), PING);
+    }
+
+    URI tilbakebetalingURI() {
+        return uri(getUri(), TILBAKEBETALING_UTTALELSE);
+    }
+}
---
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsending/pdf/TilbakebetalingUttalelseDto.java
@@ -0,0 +1,13 @@
+package no.nav.foreldrepenger.selvbetjening.tjeneste.innsending.pdf;
+
+import lombok.Data;
+
+@Data
+public class TilbakebetalingUttalelseDto {
+    private final String navn;
+    private final String fnr;
+    private final String saksnummer;
+    private final String ytelse;
+    private final String innsendtDato;
+    private final String tilsvar;
+}
