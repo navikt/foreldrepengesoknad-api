5fc8280168a8a7d6ff34b8653c68ef02b918431d
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-03-23 14:00:01 +0100

    nye typer

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/uttaksplan/UtsettelsePeriodeType.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/innsyn/uttaksplan/UtsettelsePeriodeType.java
@@ -1,5 +1,11 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.innsyn.uttaksplan;
 
 public enum UtsettelsePeriodeType {
-    ARBEID, FERIE, SYKDOM_SKADE, SØKER_INNLAGT, BARN_INNLAGT
+    ARBEID,
+    FERIE,
+    SYKDOM_SKADE,
+    SØKER_INNLAGT,
+    BARN_INNLAGT,
+    HV_OVELSE,
+    NAV_TILTAK
 }
