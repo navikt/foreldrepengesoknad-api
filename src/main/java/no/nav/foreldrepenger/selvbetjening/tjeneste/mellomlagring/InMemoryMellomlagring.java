416aea7bf3e5319b76dbacb07a421738546558a3
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-05-18 09:41:25 +0200

    test

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/mellomlagring/InMemoryMellomlagring.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/mellomlagring/InMemoryMellomlagring.java
@@ -1,15 +1,10 @@
 package no.nav.foreldrepenger.selvbetjening.tjeneste.mellomlagring;
 
-<<<<<<< master
-import static no.nav.foreldrepenger.selvbetjening.tjeneste.mellomlagring.Bøtte.SØKNAD;
-import static no.nav.foreldrepenger.selvbetjening.tjeneste.mellomlagring.Bøtte.TMP;
-
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import no.nav.foreldrepenger.boot.conditionals.ConditionalOnLocal;
@@ -20,7 +15,7 @@ public class InMemoryMellomlagring extends AbstractMellomlagringTjeneste {
 
     private final Map<String, String> store;
 
-    public InMemoryMellomlagring(@Qualifier(SØKNAD) Bøtte b1, @Qualifier(TMP) Bøtte b2) {
+    public InMemoryMellomlagring(Bøtte b1, Bøtte b2) {
         super(b1, b2);
         store = new HashMap<>();
     }
@@ -58,64 +53,8 @@ public class InMemoryMellomlagring extends AbstractMellomlagringTjeneste {
 
     @Override
     protected Optional<String> doLes(String bøtte, String katalog, String key) {
-        return Optional.ofNullable(store.get(key(katalog, key)));
-=======
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-
-import org.springframework.stereotype.Service;
-
-import no.nav.foreldrepenger.boot.conditionals.ConditionalOnLocal;
-
-@Service
-@ConditionalOnLocal
-public class InMemoryMellomlagring extends AbstractMellomlagringTjeneste {
-
-    private final Map<String, String> store;
-
-    public InMemoryMellomlagring(Bøtte b1, Bøtte b2) {
-        super(b1, b2);
-        store = new HashMap<String, String>();
-    }
-
-    @Override
-    public void slett(MellomlagringType type, String katalog, String key) {
-        store.remove(key(katalog, key));
-    }
-
-    @Override
-    public String ping() {
-        return "OK";
-    }
-
-    @Override
-    public URI pingURI() {
-        return URI.create("http://localhost/stub");
-    }
-
-    @Override
-    public String name() {
-        return pingURI().getHost();
-    }
-
-    @Override
-    protected void validerBøtte(Bøtte bøtte) {
-
-    }
-
-    @Override
-    protected void doLagre(String bøtte, String katalog, String key, String value) {
-        store.put(key(katalog, key), value);
-
-    }
-
-    @Override
-    protected Optional<String> doLes(String bøtte, String katalog, String key) {
         var val = store.get(key(katalog, key));
         return val != null ? Optional.of(val) : Optional.empty();
->>>>>>> d1fd397 sett local cluster hvis ikke NAIS
     }
 
     @Override
