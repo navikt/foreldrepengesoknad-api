6b775d0cc12d2e8ec15d8529ade54e60b296a7d4
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-06-23 10:36:38 +0200

    no retry on server exeptions

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/felles/RetryAware.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/felles/RetryAware.java
@@ -2,14 +2,17 @@ package no.nav.foreldrepenger.selvbetjening.tjeneste.felles;
 
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
-import org.springframework.web.client.HttpClientErrorException.Forbidden;
-import org.springframework.web.client.HttpServerErrorException.BadGateway;
-import org.springframework.web.client.HttpServerErrorException.InternalServerError;
-import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestClientException;
 
-@Retryable(include = { ResourceAccessException.class,
-        BadGateway.class }, exclude = { InternalServerError.class,
-                Forbidden.class }, maxAttemptsExpression = "#{${rest.retry.attempts:3}}", backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
+@Retryable(include = {
+        RestClientException.class }, exclude = {
+                HttpServerErrorException.class,
+                HttpClientErrorException.NotFound.class,
+                HttpClientErrorException.Forbidden.class,
+                HttpClientErrorException.BadRequest.class,
+                HttpClientErrorException.Unauthorized.class }, maxAttemptsExpression = "#{${rest.retry.attempts:3}}", backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
 
 public interface RetryAware {
 
