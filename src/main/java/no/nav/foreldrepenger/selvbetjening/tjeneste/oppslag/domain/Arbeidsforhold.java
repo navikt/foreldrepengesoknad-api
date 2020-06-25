365f2914cb69f8a30178ffcef85170cea3072790
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-06-25 14:26:08 +0200

    jsonAlias

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/oppslag/domain/Arbeidsforhold.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/oppslag/domain/Arbeidsforhold.java
@@ -3,6 +3,8 @@ package no.nav.foreldrepenger.selvbetjening.tjeneste.oppslag.domain;
 import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -13,8 +15,21 @@ public class Arbeidsforhold {
     private final String arbeidsgiverIdType;
     private final String arbeidsgiverNavn;
     private final Double stillingsprosent;
-    @JsonAlias("from")
     private final LocalDate fom;
-    @JsonAlias("to")
     private final LocalDate tom;
+
+    @JsonCreator
+    public Arbeidsforhold(@JsonProperty("arbeidsgiverId") String arbeidsgiverId,
+            @JsonProperty("arbeidsgiverIdType") String arbeidsgiverIdType,
+            @JsonProperty("arbeidsgiverNavn") String arbeidsgiverNavn,
+            @JsonProperty("stillingsprosent") Double stillingsprosent,
+            @JsonProperty("fom") @JsonAlias("from") LocalDate fom,
+            @JsonProperty("tom") @JsonAlias("to") LocalDate tom) {
+        this.arbeidsgiverId = arbeidsgiverId;
+        this.arbeidsgiverIdType = arbeidsgiverIdType;
+        this.arbeidsgiverNavn = arbeidsgiverNavn;
+        this.stillingsprosent = stillingsprosent;
+        this.fom = fom;
+        this.tom = tom;
+    }
 }
