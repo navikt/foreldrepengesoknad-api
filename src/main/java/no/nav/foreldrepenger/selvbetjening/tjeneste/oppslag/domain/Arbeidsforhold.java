d2c6227228d2fb4601d378d5d304783fe8e31d94
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-06-25 13:55:07 +0200

    jsonAlias

--- src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/oppslag/domain/Arbeidsforhold.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/tjeneste/oppslag/domain/Arbeidsforhold.java
@@ -2,13 +2,17 @@ package no.nav.foreldrepenger.selvbetjening.tjeneste.oppslag.domain;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 public class Arbeidsforhold {
 
     public String arbeidsgiverId;
     public String arbeidsgiverIdType;
     public String arbeidsgiverNavn;
     public Double stillingsprosent;
+    @JsonAlias("from")
     public LocalDate fom;
+    @JsonAlias("to")
     public LocalDate tom;
 
     @Override
