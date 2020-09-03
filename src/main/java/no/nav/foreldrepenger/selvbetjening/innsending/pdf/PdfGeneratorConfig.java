d225d659906a67461a3e3d38deb43282273b5369
Steffen Pøhner (NAV) <steffen.h.pohner@nav.no>
2020-09-03 19:57:08 +0200

    Fppdfgen is in default ns in sbs

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsending/pdf/PdfGeneratorConfig.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsending/pdf/PdfGeneratorConfig.java
@@ -11,9 +11,9 @@ import no.nav.foreldrepenger.selvbetjening.http.AbstractConfig;
 @ConfigurationProperties(prefix = "fppdfgen")
 public class PdfGeneratorConfig extends AbstractConfig {
 
-    private static final String DEFAULT_FPPDFGEN_URI = "http://fppdfgen.default.svc.nais.local";
-    private static final String TILBAKEBETALING_UTTALELSE = "/api/v1/genpdf/tilbakebetaling/uttalelse";
-    private static final String PING = "/is_alive";
+    private static final String DEFAULT_FPPDFGEN_URI = "http://fppdfgen.default.svc.nais.local/";
+    private static final String TILBAKEBETALING_UTTALELSE = "api/v1/genpdf/tilbakebetaling/uttalelse";
+    private static final String PING = "is_alive";
 
     @ConstructorBinding
     public PdfGeneratorConfig(@DefaultValue(DEFAULT_FPPDFGEN_URI) URI uri, @DefaultValue("true") boolean enabled) {
