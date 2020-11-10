816cb4838416d1b6d6e75e7f5a2cb865f8147141
Jan-Olav Eide <jan-olav.eide@nav.no>
2020-11-10 12:06:29 +0100

    siste rest av pdl-mekk

--- src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/OppslagConfig.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/OppslagConfig.java
@@ -1,8 +1,5 @@
 package no.nav.foreldrepenger.selvbetjening.oppslag;
 
-import static no.nav.foreldrepenger.selvbetjening.util.StringUtil.limit;
-import static org.apache.commons.lang3.StringUtils.reverse;
-
 import java.net.URI;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -17,45 +14,23 @@ public class OppslagConfig extends AbstractConfig {
     private static final String FNR = "fnr";
     private static final String PING = "actuator/health/liveness";
     private static final String PERSON = "oppslag/person";
-    private static final String SØKERINFO = "oppslag";
     private static final String AKTØRFNR = "oppslag/aktoer";
 
-    private final String key;
-    private final URI pdlUri;
-
     @ConstructorBinding
-    public OppslagConfig(URI uri, URI pdlUri, String key, @DefaultValue("true") boolean enabled) {
+    public OppslagConfig(URI uri, @DefaultValue("true") boolean enabled) {
         super(uri, enabled);
-        this.key = key;
-        this.pdlUri = pdlUri;
     }
-
-    /*
-     * @Override public String getKey() { return key; }
-     *
-     * @Override public URI zoneCrossingUri() { return getUri(); }
-     */
 
     @Override
     public URI pingURI() {
-        return uri(pdlUri, PING);
+        return uri(getUri(), PING);
     }
 
     URI personURI() {
-        return uri(pdlUri, PERSON);
-    }
-
-    URI søkerInfoURI() {
-        return uri(getUri(), SØKERINFO);
+        return uri(getUri(), PERSON);
     }
 
     URI aktørIdURI(String fnr) {
-        return uri(pdlUri, AKTØRFNR, queryParams(FNR, fnr));
+        return uri(getUri(), AKTØRFNR, queryParams(FNR, fnr));
     }
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + "[key=" + limit(reverse(key), 3) + "]";
-    }
-
 }
