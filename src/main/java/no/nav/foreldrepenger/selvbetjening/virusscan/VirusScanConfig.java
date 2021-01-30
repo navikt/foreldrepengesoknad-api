1d1ddbc1e62e19f9e1e14f3841d419614fe4f1ee
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-01-30 18:27:57 +0100

    clam

--- src/main/java/no/nav/foreldrepenger/selvbetjening/virusscan/VirusScanConfig.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/virusscan/VirusScanConfig.java
@@ -2,16 +2,18 @@ package no.nav.foreldrepenger.selvbetjening.virusscan;
 
 import java.net.URI;
 
+import org.apache.http.client.utils.URIBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 
+import lombok.SneakyThrows;
 import no.nav.foreldrepenger.selvbetjening.http.AbstractConfig;
 
 @ConfigurationProperties(prefix = "virus")
 public class VirusScanConfig extends AbstractConfig {
 
-    private static final String DEFAULT_CLAM_URI = "http://clamav.nais.svc.nais.local/scan";
+    private static final String DEFAULT_CLAM_URI = "http://clamav.nais.svc.nais./scan";
 
     @ConstructorBinding
     public VirusScanConfig(@DefaultValue(DEFAULT_CLAM_URI) URI uri, @DefaultValue("true") boolean enabled) {
@@ -19,7 +21,11 @@ public class VirusScanConfig extends AbstractConfig {
     }
 
     @Override
+    @SneakyThrows
     protected URI pingURI() {
-        return getUri();
+        return new URIBuilder()
+                .setScheme(getUri().getScheme())
+                .setHost(getUri().getHost())
+                .build();
     }
 }
