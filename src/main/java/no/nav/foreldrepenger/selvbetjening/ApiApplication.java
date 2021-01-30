fcd2bc004fe5a9d1d7aa8d56452014e1ad58528c
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-01-30 18:59:37 +0100

    disable discovery

--- src/main/java/no/nav/foreldrepenger/selvbetjening/ApiApplication.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/ApiApplication.java
@@ -6,14 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.retry.annotation.EnableRetry;
 
 import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 @EnableOpenApi
 @EnableCaching
 @EnableRetry
