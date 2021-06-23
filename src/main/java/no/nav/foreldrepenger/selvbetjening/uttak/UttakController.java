debb10d68ced4ef7da5e7022e6c9a1430a649534
Jan-Olav Eide <jan-olav.eide@nav.no>
2021-06-23 14:51:06 +0200

    @CrossOrigin

--- src/main/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakController.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import javax.inject.Inject;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,6 +39,7 @@ public class UttakController {
     }
 
     @GetMapping
+    @CrossOrigin
     public Map<String, Map<Stønadskontotype, Integer>> kontoer(
             @RequestParam("antallBarn") int antallBarn,
             @RequestParam("morHarRett") boolean morHarRett,
