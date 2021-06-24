8b8e359f3fbf522c291557972dad1b7293be7398
Andreas Dybdahl <andreas.dybdahl@nav.no>
2021-06-24 10:52:55 +0200

    Attempting to fix CORS issue

--- src/main/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakController.java
@@ -39,7 +39,7 @@ public class UttakController {
     }
 
     @GetMapping
-    @CrossOrigin(origins = "https://foreldrepengesoknad.nav.no, https://foreldrepengeplanlegger.nav.no, https://foreldrepenger.nav.no, http://localhost:8080", allowCredentials = "false")
+    @CrossOrigin(origins = "*", allowCredentials = "false")
     public Map<String, Map<Stønadskontotype, Integer>> kontoer(
             @RequestParam("antallBarn") int antallBarn,
             @RequestParam("morHarRett") boolean morHarRett,
