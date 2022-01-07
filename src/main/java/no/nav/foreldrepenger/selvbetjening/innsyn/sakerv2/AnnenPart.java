2776a56217da6458cd78e6663832302984e5cfb6
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-01-07 10:50:53 +0100

    SakerV2 oppdatere dto

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/sakerv2/AnnenPart.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/sakerv2/AnnenPart.java
@@ -1,14 +1,22 @@
 package no.nav.foreldrepenger.selvbetjening.innsyn.sakerv2;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
 import java.util.Objects;
 
 
 public record AnnenPart(PersonDetaljer personDetaljer) {
 
-    @JsonCreator
+    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
     public AnnenPart {
         Objects.requireNonNull(personDetaljer, "Persondetaljer kan ikke være null");
     }
+
+    @Override
+    @JsonValue
+    public PersonDetaljer personDetaljer() {
+        return personDetaljer;
+    }
 }
+
