9184e04d5ddf0e73b433c677dec73a1b0e573739
Espen Waaga <espen.waaga@nav.no>
2022-03-22 14:05:04 +0100

    Fjerner ubrukt setEnvironemnt

--- src/main/java/no/nav/foreldrepenger/selvbetjening/error/ApiExceptionHandler.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/error/ApiExceptionHandler.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -175,11 +174,5 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     public String toString() {
         return getClass().getSimpleName() + "[tokenUtil=" + tokenUtil + "]";
-    }
-
-    @Override
-    public void setEnvironment(Environment env) {
-        this.env = env;
-
     }
 }
