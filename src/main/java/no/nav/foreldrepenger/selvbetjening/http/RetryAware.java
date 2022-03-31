4bef5afb207e1f97b721804b9a3f5e8d7fcc64e9
Espen Waaga <espen.waaga@nav.no>
2022-03-31 13:34:14 +0200

    Skal ikke retry på request som feiler pga 400, 401, 403 og 404

--- src/main/java/no/nav/foreldrepenger/selvbetjening/http/RetryAware.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/http/RetryAware.java
@@ -8,12 +8,17 @@ import org.springframework.web.client.RestClientException;
 import org.springframework.web.multipart.MultipartException;
 
 
-@Retryable(include = {
-        RestClientException.class, MultipartException.class, HttpClientErrorException.BadRequest.class, }, exclude = {
-                HttpServerErrorException.class,
-                HttpClientErrorException.NotFound.class,
-                HttpClientErrorException.Forbidden.class,
-                HttpClientErrorException.Unauthorized.class }, maxAttemptsExpression = "#{${rest.retry.attempts:3}}", backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
+@Retryable(
+    include = {RestClientException.class, MultipartException.class, HttpClientErrorException.BadRequest.class, },
+    exclude = {
+        HttpClientErrorException.NotFound.class,
+        HttpClientErrorException.Forbidden.class,
+        HttpClientErrorException.BadRequest.class,
+        HttpClientErrorException.Unauthorized.class,
+        HttpServerErrorException.class,
+        HttpClientErrorException.NotFound.class,
+        HttpClientErrorException.Forbidden.class,
+        HttpClientErrorException.Unauthorized.class }, maxAttemptsExpression = "#{${rest.retry.attempts:3}}", backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
 
 public interface RetryAware {
 
