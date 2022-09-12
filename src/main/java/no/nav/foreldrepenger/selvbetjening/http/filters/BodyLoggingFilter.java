0d9924816cf90202d6bfa37578d03322772c2bc5
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-09-12 13:03:48 +0200

    Logg requestbody til securelog (#2270)

---
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/http/filters/BodyLoggingFilter.java
@@ -0,0 +1,43 @@
+package no.nav.foreldrepenger.selvbetjening.http.filters;
+
+import no.nav.boot.conditionals.ConditionalOnNotProd;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.AbstractRequestLoggingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static no.nav.foreldrepenger.selvbetjening.innsending.InnsendingController.INNSENDING_CONTROLLER_PATH;
+
+@Component
+@ConditionalOnNotProd
+public class BodyLoggingFilter extends AbstractRequestLoggingFilter {
+
+    private static final Logger SECURE_LOGGER = LoggerFactory.getLogger("secureLogger");
+
+    public BodyLoggingFilter() {
+        super.setIncludeClientInfo(false);
+        super.setIncludeQueryString(true);
+        super.setIncludePayload(true);
+        super.setAfterMessagePrefix("[");
+        super.setAfterMessageSuffix("]");
+        super.setMaxPayloadLength(100000);
+    }
+
+    @Override
+    protected boolean shouldLog(HttpServletRequest request) {
+        return !request.getMethod().equals("GET")
+            && request.getRequestURI().contains(INNSENDING_CONTROLLER_PATH);
+    }
+
+    @Override
+    protected void beforeRequest(HttpServletRequest request, String message) {
+        // body ikke tilgjengelig
+    }
+
+    @Override
+    protected void afterRequest(HttpServletRequest request, String message) {
+        SECURE_LOGGER.info(message);
+    }
+}
