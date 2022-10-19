2ca8bd880a99b3dfb3e385722ce073675a3a95f1
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-10-19 13:37:31 +0200

    Arkivdokumenttjeneste: legger til mediatype

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/DokumentArkivDev.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/DokumentArkivDev.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.selvbetjening.innsyn;
 
 import no.nav.boot.conditionals.ConditionalOnNotProd;
 import no.nav.foreldrepenger.selvbetjening.http.ProtectedRestController;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -18,7 +19,7 @@ public class DokumentArkivDev {
         this.dokumentArkivTjeneste = dokumentArkivTjeneste;
     }
 
-    @GetMapping("/hent-dokument")
+    @GetMapping(value = "/hent-dokument", produces = MediaType.APPLICATION_PDF_VALUE)
     public byte[] hentDokument(@RequestParam("dokumentId") String dokumentId) {
         return dokumentArkivTjeneste.hentDokument(dokumentId);
     }
