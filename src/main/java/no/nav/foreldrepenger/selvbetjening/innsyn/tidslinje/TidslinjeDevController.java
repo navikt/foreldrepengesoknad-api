d3b6b3aafba2c4c4cb9f3d5a8c0cccff837dcb42
Steffen Pøhner <steffen.h.pohner@nav.no>
2022-11-22 11:14:53 +0100

    Fix TidslinjeDevController ctor

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/tidslinje/TidslinjeDevController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/tidslinje/TidslinjeDevController.java
@@ -7,12 +7,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.inject.Inject;
+
 @ConditionalOnNotProd
 @ProtectedRestController("/rest/innsyn/tidslinje")
 public class TidslinjeDevController {
 
     private TidslinjeTjeneste tidslinjeTjeneste;
 
+    @Inject
     public TidslinjeDevController(TidslinjeTjeneste tidslinjeTjeneste) {
         this.tidslinjeTjeneste = tidslinjeTjeneste;
     }
