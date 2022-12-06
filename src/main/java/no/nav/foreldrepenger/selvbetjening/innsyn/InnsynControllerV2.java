a0627d4a342de454519226d2dc8814f054bc5e64
Anders Palfi <anderspalfi@gmail.com>
2022-12-06 08:34:12 +0100

    v2 innsyn tjeneste prod

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynControllerV2.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynControllerV2.java
@@ -10,12 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import no.nav.boot.conditionals.ConditionalOnNotProd;
 import no.nav.foreldrepenger.common.innsyn.v2.AnnenPartVedtak;
 import no.nav.foreldrepenger.common.innsyn.v2.Saker;
 import no.nav.foreldrepenger.selvbetjening.http.ProtectedRestController;
 
-@ConditionalOnNotProd
 @ProtectedRestController(INNSYN)
 public class InnsynControllerV2 {
 
