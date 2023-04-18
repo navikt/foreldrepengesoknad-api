989b147cb78a98102883e014451046d2937604f3
Anders Palfi <anders.palfi@nav.no>
2023-04-18 14:47:59 +0200

    vtp env kaller mot fpoversikt

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynTjeneste.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynTjeneste.java
@@ -1,16 +1,18 @@
 package no.nav.foreldrepenger.selvbetjening.innsyn;
 
-import no.nav.foreldrepenger.common.innsyn.AnnenPartVedtak;
-import no.nav.foreldrepenger.common.innsyn.Saker;
+import static no.nav.boot.conditionals.EnvUtil.isDevOrLocal;
+import static no.nav.boot.conditionals.EnvUtil.isVTP;
+
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
-import static no.nav.boot.conditionals.EnvUtil.isDevOrLocal;
+import no.nav.foreldrepenger.common.innsyn.AnnenPartVedtak;
+import no.nav.foreldrepenger.common.innsyn.Saker;
 
 @Service
 public class InnsynTjeneste implements Innsyn, EnvironmentAware {
@@ -30,7 +32,7 @@ public class InnsynTjeneste implements Innsyn, EnvironmentAware {
     @Override
     public Saker hentSaker() {
         try {
-            if (isDevOrLocal(env)) {
+            if (isDevOrLocal(env) || isVTP(env)) {
                 var saker = oversiktConnection.hentSaker();
                 LOG.trace("Mottatt saker fra fpoversikt: {}", saker);
             }
