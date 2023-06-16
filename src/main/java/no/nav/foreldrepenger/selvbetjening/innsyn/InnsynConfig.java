b8785c368e68ee407a6814f4c492c04c0e68663b
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-06-16 09:59:43 +0200

    Fpoversikt: Bytter fra å gå mot fpinfo til fpoversikt (#2510)
    * Fpoversikt: Bytter fra å gå mot fpinfo til fpoversikt
    
    * Ikke slå opp når fnr for annenpart ikke er oppgitt ved oppslag av annenparts vedtak

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynConfig.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynConfig.java
@@ -1,20 +1,20 @@
 package no.nav.foreldrepenger.selvbetjening.innsyn;
 
-import no.nav.foreldrepenger.selvbetjening.http.AbstractConfig;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.bind.DefaultValue;
+import static no.nav.foreldrepenger.selvbetjening.util.URIUtil.uri;
 
 import java.net.URI;
 
-import static no.nav.foreldrepenger.selvbetjening.util.URIUtil.uri;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
+import no.nav.foreldrepenger.selvbetjening.http.AbstractConfig;
+
+@Deprecated
 @ConfigurationProperties(prefix = "innsyn")
 public class InnsynConfig extends AbstractConfig {
 
     private static final String PING = "actuator/health/liveness";
     private static final String ARBEIDSFORHOLD = "innsyn/arbeidsforhold";
-    private static final String SAKER = "innsyn/v2/saker";
-    private static final String ANNEN_PART_VEDTAK = "innsyn/v2/annenPartVedtak";
 
     public InnsynConfig(URI uri, @DefaultValue("true") boolean enabled) {
         super(uri, enabled);
@@ -23,14 +23,6 @@ public class InnsynConfig extends AbstractConfig {
     @Override
     public URI pingURI() {
         return uri(getBaseUri(), PING);
-    }
-
-    URI sakerURI() {
-        return uri(getBaseUri(), SAKER);
-    }
-
-    URI annenPartVedtakURI() {
-        return uri(getBaseUri(), ANNEN_PART_VEDTAK);
     }
 
     public URI arbeidsforholdURI() {
--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynTjeneste.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynTjeneste.java
@@ -4,132 +4,42 @@ import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.EnvironmentAware;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import no.nav.boot.conditionals.EnvUtil;
 import no.nav.foreldrepenger.common.innsyn.AnnenPartVedtak;
 import no.nav.foreldrepenger.common.innsyn.Saker;
 
 @Service
-public class InnsynTjeneste implements Innsyn, EnvironmentAware {
-
-    private final InnsynConnection connectionFpinfo;
-    private final OversiktConnection connectionFpoversikt;
-    private final ObjectMapper mapper;
+public class InnsynTjeneste implements Innsyn {
+    private final OversiktConnection fpoversiktConnection;
 
     private static final Logger LOG = LoggerFactory.getLogger(InnsynTjeneste.class);
-    private static final Logger SECURE_LOGGER = LoggerFactory.getLogger("secureLogger");
-    private Environment env;
 
-    public InnsynTjeneste(InnsynConnection connectionFpinfo,
-                          OversiktConnection connectionFpoversikt,
-                          ObjectMapper mapper) {
-        this.connectionFpinfo = connectionFpinfo;
-        this.connectionFpoversikt = connectionFpoversikt;
-        this.mapper = mapper;
+    public InnsynTjeneste(OversiktConnection fpoversiktConnection) {
+        this.fpoversiktConnection = fpoversiktConnection;
     }
 
     @Override
     public Saker hentSaker() {
         LOG.info("Henter saker for pålogget bruker");
-        var sakerFraFpinfo = connectionFpinfo.hentSaker();
-        var sakerFraFpoversikt = hentSakerFraFpoversikt();
-        sammenlignSakerFraOversiktOgFpinfoFailSafe(sakerFraFpinfo, sakerFraFpoversikt);
-        return EnvUtil.isProd(env) ? sakerFraFpinfo : sakerFraFpoversikt;
-    }
-
-    private Saker hentSakerFraFpoversikt() {
-        try {
-            LOG.info("Henter saker for pålogget bruker fra fpoversikt");
-            return connectionFpoversikt.hentSaker();
-        } catch (Exception e) {
-            LOG.info("Noe gikk galt med henting av saker fra fpoversikt", e);
-            return null;
-        }
-    }
-
-    private void sammenlignSakerFraOversiktOgFpinfoFailSafe(Saker sakerFraFpinfo, Saker sakerFraFpoversikt) {
-        try {
-            if (sakerFraFpinfo.equals(sakerFraFpoversikt)) {
-                LOG.info("Ingen avvik funnet ved sammenligning av saker hentet fra fpinfo og fpoversikt");
-            } else {
-                LOG.info("Avvik funnet ved sammenligning av saker hentet fra fpinfo og fpoversikt. Ulikt antall {}",
-                    uliktAntallSaker(sakerFraFpinfo, sakerFraFpoversikt));
-                SECURE_LOGGER.info("""
-                    Avvik funnet ved sammenligning av saker hentet fra fpinfo og fpoversikt.
-                    Fpinfo {}
-                    Fpoversikt {}
-                    """, serialize(sakerFraFpinfo), serialize(sakerFraFpoversikt));
-            }
-        } catch (Exception e) {
-            LOG.info("Noe gikk galt med henting eller sammenligning av saker fra fpoversikt", e);
-        }
-    }
-
-    private boolean uliktAntallSaker(Saker sakerFraFpinfo, Saker sakerFraFpoversikt) {
-        return sakerFraFpinfo.engangsstønad().size() != sakerFraFpoversikt.engangsstønad().size()
-            || sakerFraFpinfo.foreldrepenger().size() != sakerFraFpoversikt.foreldrepenger().size()
-            || sakerFraFpinfo.svangerskapspenger().size() != sakerFraFpoversikt.svangerskapspenger().size();
+        return fpoversiktConnection.hentSaker();
     }
 
     @Override
     public Optional<AnnenPartVedtak> annenPartVedtak(AnnenPartVedtakIdentifikator annenPartVedtakIdentifikator) {
-        var annenPartVedtakFpinfo = connectionFpinfo.annenPartVedtak(annenPartVedtakIdentifikator);
-        sammenlignAnnenpartsVedtakFraOversiktOgFpinfoFailSafe(annenPartVedtakIdentifikator, annenPartVedtakFpinfo);
-        return annenPartVedtakFpinfo;
-    }
-
-    private void sammenlignAnnenpartsVedtakFraOversiktOgFpinfoFailSafe(AnnenPartVedtakIdentifikator annenPartVedtakIdentifikator, Optional<AnnenPartVedtak> annenPartVedtakFpinfo) {
-        try {
-            LOG.info("Henter vedtak for annenpart fra fpoversikt");
-            var annenpartsVedtakFpoversikt = connectionFpoversikt.hentAnnenpartsVedtak(annenPartVedtakIdentifikator);
-            if (annenPartVedtakFpinfo.isEmpty() && annenpartsVedtakFpoversikt.isEmpty()) {
-                LOG.info("Ingen avvik funnet ved sammenligning av annenparts vedtak hentet fra fpinfo og fpoversikt. Begge returnerer null");
-            } else if (annenPartVedtakFpinfo.isEmpty() || annenpartsVedtakFpoversikt.isEmpty()) {
-                if (annenPartVedtakFpinfo.isPresent()) {
-                    LOG.info("Avvik i annenparts vedtak! Fpinfo returnerer vedtak mens fpoversikt returnerer null");
-                } else {
-                    LOG.info("Avvik i annenparts vedtak! Fpoversikt returnerer vedtak mens fpinfo returnerer null");
-                }
-            } else if (annenPartVedtakFpinfo.get().equals(annenpartsVedtakFpoversikt.get())) {
-                LOG.info("Ingen avvik funnet ved sammenligning av annenparts vedtak hentet fra fpinfo og fpoversikt");
-            } else {
-                LOG.info("Avvik i annenparts vedtak! Innhold er ulikt. Sjekk secure logs for mer info");
-                SECURE_LOGGER.info("""
-                    Avvik i annenparts vedtak!
-                    Fpinfo {}
-                    Fpoversikt {}
-                    """, serialize(annenPartVedtakFpinfo.get()), serialize(annenpartsVedtakFpoversikt.get()));
-            }
-        } catch (Exception e) {
-            LOG.info("Noe gikk galt med henting eller sammenligning av annenparts vedtak fra fpoversikt", e);
+        if (annenPartVedtakIdentifikator == null || annenPartVedtakIdentifikator.annenPartFødselsnummer() == null || annenPartVedtakIdentifikator.annenPartFødselsnummer().value().isBlank()) {
+            return Optional.empty();
         }
-    }
-
-    public String serialize(Object obj) {
-        try {
-            return mapper.writerWithDefaultPrettyPrinter().writeValueAsString(obj);
-        } catch (Exception e) {
-            return obj.toString();
-        }
+        return fpoversiktConnection.hentAnnenpartsVedtak(annenPartVedtakIdentifikator);
     }
 
     @Override
     public String ping() {
-        return connectionFpinfo.ping();
-    }
-
-    @Override
-    public void setEnvironment(Environment environment) {
-        this.env = environment;
+        return fpoversiktConnection.ping();
     }
 
     @Override
     public String toString() {
-        return getClass().getSimpleName() + " [innsynConnection=" + connectionFpinfo + "]";
+        return getClass().getSimpleName() + " [innsynConnection=" + fpoversiktConnection + "]";
     }
 }
