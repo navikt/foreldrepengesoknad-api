b25492fca225b018694eb1a78f7af15c55902ea6
Michal J. Sladek <michal.sladek@nav.no>
2023-08-31 16:38:33 +0200

    Tillater både gammelt og nytt acr claim.

--- src/main/java/no/nav/foreldrepenger/selvbetjening/http/ProtectedRestController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/http/ProtectedRestController.java
@@ -1,25 +1,24 @@
 package no.nav.foreldrepenger.selvbetjening.http;
 
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-
-import no.nav.foreldrepenger.common.util.TokenUtil;
+import no.nav.security.token.support.core.api.ProtectedWithClaims;
+import no.nav.security.token.support.core.api.RequiredIssuers;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import no.nav.security.token.support.core.api.ProtectedWithClaims;
-import no.nav.security.token.support.core.api.RequiredIssuers;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static no.nav.foreldrepenger.common.util.TokenUtil.*;
 
 @RestController
 @Documented
 @RequiredIssuers(
-        @ProtectedWithClaims(issuer = TokenUtil.IDPORTEN, claimMap = TokenUtil.IDPORTENV2_CLAIMS)
+        @ProtectedWithClaims(issuer = IDPORTEN, claimMap = { IDPORTENV2_CLAIMS, CLAIMS }, combineWithOr = true)
 )
 @Validated
 @Target(TYPE)
