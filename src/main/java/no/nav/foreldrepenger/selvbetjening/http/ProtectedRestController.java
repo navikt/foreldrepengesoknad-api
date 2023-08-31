f1523a68a56edaad9b32916b1b3b975ab640b2ef
Michal J. Sladek <michal.sladek@nav.no>
2023-08-31 10:15:10 +0200

    Fjerner loginservice.

--- src/main/java/no/nav/foreldrepenger/selvbetjening/http/ProtectedRestController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/http/ProtectedRestController.java
@@ -1,27 +1,26 @@
 package no.nav.foreldrepenger.selvbetjening.http;
 
-import no.nav.security.token.support.core.api.ProtectedWithClaims;
-import no.nav.security.token.support.core.api.RequiredIssuers;
-import org.springframework.core.annotation.AliasFor;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static no.nav.foreldrepenger.common.util.TokenUtil.CLAIMS;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
-import static no.nav.foreldrepenger.common.util.TokenUtil.CLAIMS;
-import static no.nav.foreldrepenger.common.util.TokenUtil.SELVBETJENING;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import no.nav.security.token.support.core.api.ProtectedWithClaims;
+import no.nav.security.token.support.core.api.RequiredIssuers;
 
 @RestController
 @Documented
-@RequiredIssuers({
-        @ProtectedWithClaims(issuer = SELVBETJENING, claimMap = CLAIMS),
-        @ProtectedWithClaims(issuer = "idporten", claimMap =  CLAIMS)
-})
+@RequiredIssuers(
+        @ProtectedWithClaims(issuer = "idporten", claimMap = CLAIMS)
+)
 @Validated
 @Target(TYPE)
 @Retention(RUNTIME)
