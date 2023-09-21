939158960390269f47ee2f070fdf71904f7eb0ef
Anders Palfi <anderspalfi@gmail.com>
2023-09-21 11:00:06 +0200

    Prodsetter tilbakekreving minidialog fra fpoversikt (#2559)

--- src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/TilbakekrevingsInnslag.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/TilbakekrevingsInnslag.java
@@ -1,6 +1,8 @@
 package no.nav.foreldrepenger.selvbetjening.innsyn;
 
+import java.time.LocalDate;
+
 import no.nav.foreldrepenger.common.domain.Saksnummer;
 
-public record TilbakekrevingsInnslag(Saksnummer saksnummer) {
+public record TilbakekrevingsInnslag(Saksnummer saksnummer, LocalDate opprettet) {
 }
