fc0c4e252cc2ed6bd105b1c035cdccfa79b5757b
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2023-09-26 09:40:53 +0200

    Henter manglende vedlegg fra fpoversikt og fjerner sammenligning (#2568)

--- src/main/java/no/nav/foreldrepenger/selvbetjening/historikk/HistorikkController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/historikk/HistorikkController.java
@@ -2,6 +2,8 @@ package no.nav.foreldrepenger.selvbetjening.historikk;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,39 +18,19 @@ import no.nav.foreldrepenger.selvbetjening.innsyn.Innsyn;
 public class HistorikkController {
 
     private static final Logger LOG = LoggerFactory.getLogger(HistorikkController.class);
-
     static final String HISTORIKK = "/rest/historikk";
 
-    private final HistorikkTjeneste historikk;
     private final Innsyn innsyn;
 
-    public HistorikkController(HistorikkTjeneste historikk, Innsyn innsyn) {
-        this.historikk = historikk;
+    public HistorikkController(Innsyn innsyn) {
         this.innsyn = innsyn;
     }
 
     @GetMapping(path = "/vedlegg")
-    public List<String> vedlegg(@Valid @RequestParam("saksnummer") Saksnummer saksnummer) {
-        var manglendeVedleggFraFpinfoHistorikk = historikk.manglendeVedlegg(saksnummer);
-        sammenlign(saksnummer, manglendeVedleggFraFpinfoHistorikk);
-        return manglendeVedleggFraFpinfoHistorikk;
-    }
-
-    private void sammenlign(Saksnummer saksnummer, List<String> fraHistorikk) {
-        try {
-            var fraFpoversikt = innsyn.hentManglendeVedlegg(saksnummer);
-            if (fraHistorikk.size() == fraFpoversikt.size() && fraHistorikk.containsAll(fraFpoversikt) && fraFpoversikt.containsAll(fraHistorikk)) {
-                LOG.info("Ingen avvik i manglende vedlegg mottatt fra fpoversikt sammenlignet med fpinfo-historikk");
-            } else {
-                LOG.info("AVVIK[manglende vedlegg]: Fpinfo-historikk returnerte {}, mens fpoversikt returnerte {} på saksnummer {}", fraHistorikk, fraFpoversikt, saksnummer.value());
-            }
-        } catch (Exception e) {
-            LOG.info("Noe gikk galt med sammenligning av manglende vedlegg", e);
-        }
-    }
-
-    @Override
-    public String toString() {
-        return getClass().getSimpleName() + " [historikk=" + historikk + "]";
+    public List<String> vedlegg(@RequestParam("saksnummer") @Valid @NotNull Saksnummer saksnummer) {
+        LOG.info("Henter manglende vedlegg for sak {}", saksnummer.value());
+        var manglendeVedlegg = innsyn.hentManglendeVedlegg(saksnummer);
+        LOG.info("Sak {} mangler følgende vedlegg: {}", saksnummer.value(), manglendeVedlegg);
+        return manglendeVedlegg;
     }
 }
