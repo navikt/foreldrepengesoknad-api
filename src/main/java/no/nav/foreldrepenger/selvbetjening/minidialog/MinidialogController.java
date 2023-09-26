b0286f8543cfa063faf5f9f708cfbab66a423a7a
Anders Palfi <anders.palfi@nav.no>
2023-09-26 07:43:47 +0200

    frist på minidialog

--- src/main/java/no/nav/foreldrepenger/selvbetjening/minidialog/MinidialogController.java
+++ src/main/java/no/nav/foreldrepenger/selvbetjening/minidialog/MinidialogController.java
@@ -2,6 +2,7 @@ package no.nav.foreldrepenger.selvbetjening.minidialog;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import no.nav.foreldrepenger.selvbetjening.innsyn.TilbakekrevingsInnslag;
 public class MinidialogController {
 
     static final String MINIDIALOG_PATH = "/rest/minidialog";
+    private static final Period TILBAKEKREVING_FRIST = Period.ofWeeks(2); //TODO skal flyttes til fptilbake
     private final Innsyn innsyn;
 
     @Autowired
@@ -30,9 +32,9 @@ public class MinidialogController {
     private static MinidialogInnslag map(TilbakekrevingsInnslag tilbakekrevingsInnslag) {
         var opprettet = tilbakekrevingsInnslag.opprettet().atTime(7, 0); //Viser bare dato i frontend
         return new MinidialogInnslag(tilbakekrevingsInnslag.saksnummer().value(), opprettet, LocalDate.now().plusWeeks(1),
-            tilbakekrevingsInnslag.saksnummer().value());
+            tilbakekrevingsInnslag.saksnummer().value(), opprettet.plus(TILBAKEKREVING_FRIST).toLocalDate());
     }
 
-    public record MinidialogInnslag(String saksnr, LocalDateTime opprettet, LocalDate gyldigTil, String dialogId) {
+    public record MinidialogInnslag(String saksnr, LocalDateTime opprettet, LocalDate gyldigTil, String dialogId, LocalDate frist) {
     }
 }
