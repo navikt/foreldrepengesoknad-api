bfca2ef1b42fffc5ade07304f2f93d8d1fa692de
espenwaaga <espen.waaga@nav.no>
2023-10-27 09:00:29 +0200

    Legger bare til hva som skal retryes på

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/http/RetryAware.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/http/RetryAware.java
@@ -2,26 +2,17 @@ package no.nav.foreldrepenger.selvbetjening.http;
 
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.multipart.MultipartException;
-
-import no.nav.foreldrepenger.selvbetjening.error.IdMismatchException;
-import no.nav.security.token.support.core.exceptions.JwtTokenInvalidClaimException;
-import no.nav.security.token.support.spring.validation.interceptor.JwtTokenUnauthorizedException;
 
 
 @Retryable(
     include = {
-        RestClientException.class,
+        HttpServerErrorException.class,
         MultipartException.class },
-    exclude = {
-        IdMismatchException.class,
-        JwtTokenUnauthorizedException.class,
-        JwtTokenInvalidClaimException.class,
-        HttpClientErrorException.class},
-    maxAttemptsExpression = "#{${rest.retry.attempts:3}}",
-    backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}"))
+    maxAttemptsExpression = "#{${rest.retry.attempts:2}}",
+    backoff = @Backoff(delayExpression = "#{${rest.retry.delay:1000}}")
+)
 public interface RetryAware {
 
 }
