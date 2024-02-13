979580fb5326afcf9e52c28a2ddf777c7e0b4a0d
espenwaaga <espen.waaga@nav.no>
2024-02-13 08:42:37 +0100

    Oppslag av søker info returnerer tom liste istedenfor å fjernet feltet

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/domain/PersonFrontend.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/domain/PersonFrontend.java
@@ -1,6 +1,6 @@
 package no.nav.foreldrepenger.selvbetjening.oppslag.domain;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 import static no.nav.foreldrepenger.common.util.StringUtil.mask;
 
 import java.time.LocalDate;
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 
 import no.nav.foreldrepenger.common.domain.Fødselsnummer;
 
-@JsonInclude(NON_EMPTY)
+@JsonInclude(NON_NULL)
 public record PersonFrontend(Fødselsnummer fnr,
                              String fornavn,
                              String mellomnavn,
--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/domain/Søkerinfo.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/domain/Søkerinfo.java
@@ -1,12 +1,12 @@
 package no.nav.foreldrepenger.selvbetjening.oppslag.domain;
 
-import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_EMPTY;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonInclude(NON_EMPTY)
+@JsonInclude(NON_NULL)
 public record Søkerinfo(PersonFrontend søker, List<Arbeidsforhold> arbeidsforhold) {
 
 }
