30a1453b9cf21fab4ad2939008eb6dd0380839d8
espenwaaga <espen.waaga@nav.no>
2024-03-12 10:51:02 +0100

    Kontoberegning: Bytter til POST endepunkt og introduserer DTO og returnere en map av kontoberegning for 80 og 100 prosent dekningsgrad

--- domene/src/test/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakControllerTest.java
+++ domene/src/test/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakControllerTest.java
@@ -20,14 +20,17 @@ class UttakControllerTest {
         LocalDate omsorgsovertakelseDato = null;
 
         assertThrows(ManglendeFamiliehendelseException.class,
-            () -> controller.beregn(1, true, true, false, false, fødselsdato, termindato, omsorgsovertakelseDato, "80", true,
+            () -> controller.beregnMedDekningsgrad(1, true, true, false, false, fødselsdato, termindato, omsorgsovertakelseDato, "80", true,
                 true, false, false, null));
+        assertThrows(ManglendeFamiliehendelseException.class,
+            () -> controller.beregn(new KontoBeregningGrunnlagDto(1, true, true, false, false, fødselsdato, termindato, omsorgsovertakelseDato, true,
+                true, false, false, null)));
     }
 
     @Test
     void bfhr() {
         var controller = new UttakController();
-        var resultat = controller.beregn(1, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
+        var resultat = controller.beregnMedDekningsgrad(1, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
             null, "100", false, true, false, false, null);
 
         assertThat(resultat.kontoer()).containsEntry(StønadskontoBeregningStønadskontotype.FORELDREPENGER, 200);
@@ -35,22 +38,41 @@ class UttakControllerTest {
         assertThat(resultat.minsteretter().farRundtFødsel()).isEqualTo(10);
 
     }
+    @Test
+    void bfhrNY() {
+        var controller = new UttakController();
+        var grunnlag = new KontoBeregningGrunnlagDto(1, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1), null, false, true,
+            false, false, null);
+        var resultat = controller.beregn(grunnlag);
+
+
+        assertThat(resultat).containsOnlyKeys("80", "100");
+        var kontoberegning100 = resultat.get("100");
+        assertThat(kontoberegning100.kontoer()).containsEntry(StønadskontoBeregningStønadskontotype.FORELDREPENGER, 200);
+        assertThat(kontoberegning100.minsteretter().generellMinsterett()).isEqualTo(40);
+        assertThat(kontoberegning100.minsteretter().farRundtFødsel()).isEqualTo(10);
+
+        var kontoberegning80 = resultat.get("80");
+        assertThat(kontoberegning80.kontoer()).containsEntry(StønadskontoBeregningStønadskontotype.FORELDREPENGER, 250);
+        assertThat(kontoberegning80.minsteretter().generellMinsterett()).isEqualTo(40);
+        assertThat(kontoberegning80.minsteretter().farRundtFødsel()).isEqualTo(10);
+
+    }
 
     @Test
     void aleneomsorg() {
         var controller = new UttakController();
-        var resultat = controller.beregn(1, false, true, false, true, null, LocalDate.now(),
+        var resultat = controller.beregnMedDekningsgrad(1, false, true, false, true, null, LocalDate.now(),
             null, "100", false, true, false, false, null);
 
         assertThat(resultat.kontoer()).containsEntry(StønadskontoBeregningStønadskontotype.FORELDREPENGER, 46 * 5);
         assertThat(resultat.minsteretter().generellMinsterett()).isZero();
-
     }
 
     @Test
     void bfhr_flere_barn_minsterett_skal_ikke_ha_flerbarnsdager() {
         var controller = new UttakController();
-        var resultat = controller.beregn(2, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
+        var resultat = controller.beregnMedDekningsgrad(2, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
             null, "100", false, true, false, false, null);
 
         assertThat(resultat.kontoer()).containsEntry(StønadskontoBeregningStønadskontotype.FORELDREPENGER, 285);
@@ -63,7 +85,7 @@ class UttakControllerTest {
     @Test
     void morEøs() {
         var controller = new UttakController();
-        var resultat = controller.beregn(1, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
+        var resultat = controller.beregnMedDekningsgrad(1, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
             null, "100", false, true, false, true, null);
 
         assertThat(resultat.kontoer()).doesNotContainKey(StønadskontoBeregningStønadskontotype.FORELDREPENGER);
