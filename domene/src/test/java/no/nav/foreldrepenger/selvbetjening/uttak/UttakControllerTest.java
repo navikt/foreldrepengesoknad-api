d1d429f1f2329592d0edd71325a5709b45045131
espenwaaga <espen.waaga@nav.no>
2024-04-30 12:44:25 +0200

    Rydder bort deprecated endepunkt for beregning av konto for søknadsdialogen

--- domene/src/test/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakControllerTest.java
+++ domene/src/test/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakControllerTest.java
@@ -1,7 +1,11 @@
 package no.nav.foreldrepenger.selvbetjening.uttak;
 
 import static no.nav.foreldrepenger.selvbetjening.uttak.KontoBeregningDto.KontoDto.KontoType.AKTIVITETSFRI_KVOTE;
+import static no.nav.foreldrepenger.selvbetjening.uttak.KontoBeregningDto.KontoDto.KontoType.FEDREKVOTE;
+import static no.nav.foreldrepenger.selvbetjening.uttak.KontoBeregningDto.KontoDto.KontoType.FELLESPERIODE;
 import static no.nav.foreldrepenger.selvbetjening.uttak.KontoBeregningDto.KontoDto.KontoType.FORELDREPENGER;
+import static no.nav.foreldrepenger.selvbetjening.uttak.KontoBeregningDto.KontoDto.KontoType.FORELDREPENGER_FØR_FØDSEL;
+import static no.nav.foreldrepenger.selvbetjening.uttak.KontoBeregningDto.KontoDto.KontoType.MØDREKVOTE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -15,6 +19,7 @@ import no.nav.foreldrepenger.stønadskonto.regelmodell.grunnlag.Brukerrolle;
 import no.nav.foreldrepenger.stønadskonto.regelmodell.grunnlag.Rettighetstype;
 
 
+
 class UttakControllerTest {
 
     private Environment environment = new MockEnvironment();
@@ -23,54 +28,60 @@ class UttakControllerTest {
     @Test
     void skal_kaste_exception_hvis_manglende_familiehendelse() {
         var controller = new UttakController(uttakCore2024);
-        LocalDate fødselsdato = null;
-        LocalDate termindato = null;
-        LocalDate omsorgsovertakelseDato = null;
+        var grunnlag = new KontoBeregningGrunnlagDto(
+            Rettighetstype.BEGGE_RETT,
+            Brukerrolle.MOR,
+            1,
+            null,
+            null,
+            null,
+            false,
+            null);
+        assertThrows(ManglendeFamiliehendelseException.class, () -> controller.beregn(grunnlag));
+    }
 
-        assertThrows(ManglendeFamiliehendelseException.class,
-            () -> controller.beregnMedDekningsgrad(
-                1,
-                true,
-                true,
-                false,
-                false,
-                fødselsdato,
-                termindato,
-                omsorgsovertakelseDato,
-                "80",
-                true,
-                true,
-                false,
-                false,
-                null));
-        assertThrows(ManglendeFamiliehendelseException.class,
-            () -> controller.beregn(
-                new KontoBeregningGrunnlagDto(
-                    Rettighetstype.BEGGE_RETT,
-                    Brukerrolle.MOR,
-                    1,
-                    fødselsdato,
-                    termindato,
-                    omsorgsovertakelseDato,
-                    false,
-                    null
-                    )
-            ));
+    @Test
+    void mor_og_far_rett_to_tette() {
+        var controller = new UttakController();
+        var grunnlag = new KontoBeregningGrunnlagDto(
+            Rettighetstype.BEGGE_RETT,
+            Brukerrolle.MOR,
+            1,
+            LocalDate.now().minusMonths(7),
+            LocalDate.now().minusMonths(7).minusWeeks(1),
+            null,
+            false,
+            LocalDate.now()
+        );
+        var resultat = controller.beregn(grunnlag);
+
+        assertThat(resultat).containsOnlyKeys("80", "100");
+        var kontoberegning100 = resultat.get("100");
+        assertThat(kontoberegning100.kontoer()).containsExactlyInAnyOrder(
+            new KontoBeregningDto.KontoDto(FORELDREPENGER_FØR_FØDSEL, 15),
+            new KontoBeregningDto.KontoDto(MØDREKVOTE, 75),
+            new KontoBeregningDto.KontoDto(FEDREKVOTE, 75),
+            new KontoBeregningDto.KontoDto(FELLESPERIODE, 80)
+        );
+
+        var kontoberegning80 = resultat.get("80");
+        assertThat(kontoberegning80.kontoer()).containsExactlyInAnyOrder(
+            new KontoBeregningDto.KontoDto(FORELDREPENGER_FØR_FØDSEL, 15),
+            new KontoBeregningDto.KontoDto(MØDREKVOTE, 95),
+            new KontoBeregningDto.KontoDto(FEDREKVOTE, 95),
+            new KontoBeregningDto.KontoDto(FELLESPERIODE, 90)
+        );
+
+        assertThat(kontoberegning100.minsteretter().farRundtFødsel())
+            .isEqualTo(kontoberegning80.minsteretter().farRundtFødsel())
+            .isEqualTo(10);
+        assertThat(kontoberegning100.minsteretter().toTette())
+            .isEqualTo(kontoberegning80.minsteretter().toTette())
+            .isEqualTo(110);
     }
 
     @Test
     void bfhr() {
-        var controller = new UttakController(uttakCore2024);
-        var resultat = controller.beregnMedDekningsgrad(1, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
-            null, "100", false, true, false, false, null);
-
-        assertThat(resultat.kontoer()).containsEntry(KontoBeregning.StønadskontoBeregningStønadskontotype.FORELDREPENGER, 200);
-        assertThat(resultat.minsteretter().generellMinsterett()).isEqualTo(40);
-        assertThat(resultat.minsteretter().farRundtFødsel()).isEqualTo(10);
-
-    }
-    @Test
-    void bfhrNY() {
         var controller = new UttakController(uttakCore2024);
         var grunnlag = new KontoBeregningGrunnlagDto(
             Rettighetstype.BARE_SØKER_RETT,
@@ -84,53 +95,82 @@ class UttakControllerTest {
         );
         var resultat = controller.beregn(grunnlag);
 
-
         assertThat(resultat).containsOnlyKeys("80", "100");
         var kontoberegning100 = resultat.get("100");
         assertThat(kontoberegning100.kontoer()).contains(new KontoBeregningDto.KontoDto(FORELDREPENGER, 160));
         assertThat(kontoberegning100.kontoer()).contains(new KontoBeregningDto.KontoDto(AKTIVITETSFRI_KVOTE, 40));
-        assertThat(kontoberegning100.minsteretter().farRundtFødsel()).isEqualTo(10);
 
         var kontoberegning80 = resultat.get("80");
         assertThat(kontoberegning80.kontoer()).contains(new KontoBeregningDto.KontoDto(FORELDREPENGER, 210));
         assertThat(kontoberegning100.kontoer()).contains(new KontoBeregningDto.KontoDto(AKTIVITETSFRI_KVOTE, 40));
-        assertThat(kontoberegning80.minsteretter().farRundtFødsel()).isEqualTo(10);
-
+        assertThat(kontoberegning100.minsteretter().farRundtFødsel())
+            .isEqualTo(kontoberegning80.minsteretter().farRundtFødsel())
+            .isEqualTo(10);
     }
 
     @Test
-    void aleneomsorg() {
+    void mor_aleneomsorg() {
         var controller = new UttakController(uttakCore2024);
-        var resultat = controller.beregnMedDekningsgrad(1, false, true, false, true, null, LocalDate.now(),
-            null, "100", false, true, false, false, null);
+        var grunnlag = new KontoBeregningGrunnlagDto(
+            Rettighetstype.ALENEOMSORG,
+            Brukerrolle.MOR,
+            1,
+            LocalDate.now(),
+            LocalDate.now().minusWeeks(1),
+            null,
+            false,
+            null
+        );
+        var resultat = controller.beregn(grunnlag);
 
-        assertThat(resultat.kontoer()).containsEntry(KontoBeregning.StønadskontoBeregningStønadskontotype.FORELDREPENGER, 46 * 5);
-        assertThat(resultat.minsteretter().generellMinsterett()).isZero();
+        assertThat(resultat).containsOnlyKeys("80", "100");
+        var kontoberegning100 = resultat.get("100");
+        assertThat(kontoberegning100.kontoer()).containsExactlyInAnyOrder(
+            new KontoBeregningDto.KontoDto(FORELDREPENGER_FØR_FØDSEL, 15),
+            new KontoBeregningDto.KontoDto(FORELDREPENGER, 230)
+        );
+
+        var kontoberegning80 = resultat.get("80");
+        assertThat(kontoberegning80.kontoer()).containsExactlyInAnyOrder(
+            new KontoBeregningDto.KontoDto(FORELDREPENGER_FØR_FØDSEL, 15),
+            new KontoBeregningDto.KontoDto(FORELDREPENGER, 280)
+            );
+
+        assertThat(kontoberegning100.minsteretter().farRundtFødsel())
+            .isEqualTo(kontoberegning80.minsteretter().farRundtFødsel())
+            .isZero();
     }
 
     @Test
     void bfhr_flere_barn_minsterett_skal_ikke_ha_flerbarnsdager() {
         var controller = new UttakController(uttakCore2024);
-        var resultat = controller.beregnMedDekningsgrad(2, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
-            null, "100", false, true, false, false, null);
+        var grunnlag = new KontoBeregningGrunnlagDto(
+            Rettighetstype.BARE_SØKER_RETT,
+            Brukerrolle.FAR,
+            2,
+            null,
+            LocalDate.now().minusWeeks(1),
+            null,
+            false,
+            null
+        );
+        var resultat = controller.beregn(grunnlag);
 
-        assertThat(resultat.kontoer()).containsEntry(KontoBeregning.StønadskontoBeregningStønadskontotype.FORELDREPENGER, 285);
-        assertThat(resultat.kontoer()).doesNotContainKey(KontoBeregning.StønadskontoBeregningStønadskontotype.FLERBARNSDAGER);
-        assertThat(resultat.minsteretter().generellMinsterett()).isEqualTo(85);
-        assertThat(resultat.minsteretter().farRundtFødsel()).isEqualTo(10);
 
+        assertThat(resultat).containsOnlyKeys("80", "100");
+        var kontoberegning100 = resultat.get("100");
+        assertThat(kontoberegning100.kontoer()).containsExactlyInAnyOrder(
+            new KontoBeregningDto.KontoDto(AKTIVITETSFRI_KVOTE, 85),
+            new KontoBeregningDto.KontoDto(FORELDREPENGER, 200)
+        );
+
+        var kontoberegning80 = resultat.get("80");
+        assertThat(kontoberegning80.kontoer()).containsExactlyInAnyOrder(
+            new KontoBeregningDto.KontoDto(AKTIVITETSFRI_KVOTE, 105),
+            new KontoBeregningDto.KontoDto(FORELDREPENGER, 250)
+        );
+        assertThat(kontoberegning100.minsteretter().farRundtFødsel())
+            .isEqualTo(kontoberegning80.minsteretter().farRundtFødsel())
+            .isEqualTo(10);
     }
-
-    @Test
-    void morEøs() {
-        var controller = new UttakController(uttakCore2024);
-        var resultat = controller.beregnMedDekningsgrad(1, false, true, false, false, LocalDate.now(), LocalDate.now().minusWeeks(1),
-            null, "100", false, true, false, true, null);
-
-        assertThat(resultat.kontoer()).doesNotContainKey(KontoBeregning.StønadskontoBeregningStønadskontotype.FORELDREPENGER);
-        assertThat(resultat.minsteretter().generellMinsterett()).isZero();
-        assertThat(resultat.minsteretter().farRundtFødsel()).isEqualTo(10);
-        assertThat(resultat.kontoer()).containsEntry(KontoBeregning.StønadskontoBeregningStønadskontotype.FEDREKVOTE, 75);
-    }
-
 }
