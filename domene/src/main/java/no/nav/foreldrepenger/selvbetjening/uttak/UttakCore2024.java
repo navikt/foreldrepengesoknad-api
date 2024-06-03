1417f42f02b72c7cd5ee7497c9fccb2616114f5a
espenwaaga <espen.waaga@nav.no>
2024-06-03 14:24:42 +0200

    TFP-5769: Justere på rammeverk for ikraftredelse av nye lovendringer i DEV

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakCore2024.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakCore2024.java
@@ -12,17 +12,19 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UttakCore2024 {
-    private static LocalDate DEFAULT_IKRAFTTREDELSEDATO_1 = LocalDate.of(2024, Month.JULY,1); // LA STÅ etter endring til 2024
-    private static LocalDate DEFAULT_IKRAFTTREDELSEDATO_2 = LocalDate.of(2024, Month.AUGUST,2); // LA STÅ.
+    public static final LocalDate DEFAULT_IKRAFTTREDELSEDATO_1 = LocalDate.of(2024, Month.JULY,1); // LA STÅ etter endring til 2024
+    public static final LocalDate DEFAULT_IKRAFTTREDELSEDATO_2 = LocalDate.of(2024, Month.AUGUST,2); // LA STÅ.
 
     private LocalDate ikrafttredelseDato1;
     private LocalDate ikrafttredelseDato2;
+    private Environment env;
 
     public UttakCore2024(@Value("${dato.for.aatti.prosent:#{null}}") LocalDate overstyrtIkrafttredelsedato1,
                          @Value("${dato.for.minsterett.andre:#{null}}") LocalDate overstyrtIkrafttredelsedato2,
                          Environment env) {
-        ikrafttredelseDato1 = isProd(env) || overstyrtIkrafttredelsedato1 == null ? DEFAULT_IKRAFTTREDELSEDATO_1 : overstyrtIkrafttredelsedato1;
-        ikrafttredelseDato2 = isProd(env) || overstyrtIkrafttredelsedato2 == null ? DEFAULT_IKRAFTTREDELSEDATO_2 : overstyrtIkrafttredelsedato2;
+        this.env = env;
+        this.ikrafttredelseDato1 = isProd(env) || overstyrtIkrafttredelsedato1 == null ? DEFAULT_IKRAFTTREDELSEDATO_1 : overstyrtIkrafttredelsedato1;
+        this.ikrafttredelseDato2 = isProd(env) || overstyrtIkrafttredelsedato2 == null ? DEFAULT_IKRAFTTREDELSEDATO_2 : overstyrtIkrafttredelsedato2;
     }
 
     public LocalDate getIkrafttredelseDato1() {
@@ -34,7 +36,26 @@ public class UttakCore2024 {
     }
 
     public LocalDate utledRegelvalgsdato(LocalDate familieHendelseDato) {
-        if (familieHendelseDato == null || familieHendelseDato.isBefore(ikrafttredelseDato1)) return null;
+        if (familieHendelseDato == null) {
+            return null;
+        }
+        return isProd(env) ? utledRegelvalgDato(familieHendelseDato) : utledRegelvalgDatoForDev(familieHendelseDato);
+    }
+
+    private LocalDate utledRegelvalgDatoForDev(LocalDate familieHendelseDato) {
+        if (!familieHendelseDato.isBefore(ikrafttredelseDato2)) {
+            return DEFAULT_IKRAFTTREDELSEDATO_2;
+        }
+        if (!familieHendelseDato.isBefore(ikrafttredelseDato1)) {
+            return DEFAULT_IKRAFTTREDELSEDATO_1;
+        }
+        return null;
+    }
+
+    private LocalDate utledRegelvalgDato(LocalDate familieHendelseDato) {
+        if (familieHendelseDato.isBefore(ikrafttredelseDato1)) {
+            return null;
+        }
         return LocalDate.now().isBefore(ikrafttredelseDato2) ? LocalDate.now() : null;
     }
 }
--- domene/src/test/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakCore2024Test.java
+++ domene/src/test/java/no/nav/foreldrepenger/selvbetjening/uttak/UttakCore2024Test.java
@@ -15,27 +15,26 @@ class UttakCore2024Test {
         var environment = new MockEnvironment();
         environment.setActiveProfiles("prod-gcp");
 
-        var uttakCore2024 = new UttakCore2024(LocalDate.now(), LocalDate.now(), environment);
-        assertThat(uttakCore2024.getIkrafttredelseDato1()).isNotEqualTo(LocalDate.now());
-        assertThat(uttakCore2024.getIkrafttredelseDato2()).isNotEqualTo(LocalDate.now());
+        var ikrafttredelse1 = LocalDate.of(2024, 3, 1);
+        var ikrafttredelse2 = LocalDate.of(2024, 3, 1);
+        var uttakCore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, environment);
+
+        assertThat(uttakCore2024.getIkrafttredelseDato1())
+            .isNotEqualTo(ikrafttredelse1)
+            .isEqualTo(UttakCore2024.DEFAULT_IKRAFTTREDELSEDATO_1);
+        assertThat(uttakCore2024.getIkrafttredelseDato2())
+            .isNotEqualTo(ikrafttredelse2)
+            .isEqualTo(UttakCore2024.DEFAULT_IKRAFTTREDELSEDATO_2);
     }
-
-    @Test
-    void skalOverskriveVerdierIdev() {
-        var environment = new MockEnvironment();
-        environment.setActiveProfiles("dev-gcp");
-
-        var uttakCore2024 = new UttakCore2024(LocalDate.now(), LocalDate.now(), environment);
-        assertThat(uttakCore2024.getIkrafttredelseDato1()).isEqualTo(LocalDate.now());
-        assertThat(uttakCore2024.getIkrafttredelseDato2()).isEqualTo(LocalDate.now());
-    }
-
 
     @Test
     void dagensDatoErFørIkrafttredelsesdatoene() {
+        var environment = new MockEnvironment();
+        environment.setActiveProfiles("dev-gcp");
         var ikrafttredelse1 = LocalDate.now().plusMonths(2);
         var ikrafttredelse2 = LocalDate.now().plusMonths(4);
-        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, new MockEnvironment());
+        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, environment);
+        environment.setActiveProfiles("prod-gcp");
 
         var familiehendelseFør = ikrafttredelse1.minusMonths(1);
         assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseFør)).isNull();
@@ -49,9 +48,12 @@ class UttakCore2024Test {
 
     @Test
     void dagensDatoEtterFørsteIkrafttredelsesdatoMenFørAndre() {
+        var environment = new MockEnvironment();
+        environment.setActiveProfiles("dev-gcp");
         var ikrafttredelse1 = LocalDate.now().minusMonths(1);
         var ikrafttredelse2 = LocalDate.now().plusMonths(1);
-        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, new MockEnvironment());
+        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, environment);
+        environment.setActiveProfiles("prod-gcp");
 
         var familiehendelseFør = ikrafttredelse1.minusMonths(1);
         assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseFør)).isNull();
@@ -65,9 +67,12 @@ class UttakCore2024Test {
 
     @Test
     void dagensDatoEtterIkrafttredelsesdatoene() {
+        var environment = new MockEnvironment();
+        environment.setActiveProfiles("dev-gcp");
         var ikrafttredelse1 = LocalDate.now().minusMonths(4);
         var ikrafttredelse2 = LocalDate.now().minusMonths(2);
-        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, new MockEnvironment());
+        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, environment);
+        environment.setActiveProfiles("prod-gcp");
 
         var familiehendelseFør = ikrafttredelse1.minusMonths(1);
         assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseFør)).isNull();
@@ -77,5 +82,78 @@ class UttakCore2024Test {
 
         var familiehendelseEtter = ikrafttredelse2.plusMonths(1);
         assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseEtter)).isNull();
+    }
+
+
+
+    @Test
+    void skalOverskriveVerdierIdev() {
+        var environment = new MockEnvironment();
+        environment.setActiveProfiles("dev-gcp");
+
+        var ikrafttredelse1 = LocalDate.of(2024, 3, 1);
+        var ikrafttredelse2 = LocalDate.of(2024, 3, 1);
+        var uttakCore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse1, environment);
+
+        assertThat(uttakCore2024.getIkrafttredelseDato1()).isEqualTo(ikrafttredelse1);
+        assertThat(uttakCore2024.getIkrafttredelseDato2()).isEqualTo(ikrafttredelse2);
+    }
+
+
+    @Test
+    void dagensDatoErFørIkrafttredelsesdatoeneDEV() {
+        var environment = new MockEnvironment();
+        environment.setActiveProfiles("dev-gcp");
+
+        var ikrafttredelse1 = LocalDate.now().plusMonths(2);
+        var ikrafttredelse2 = LocalDate.now().plusMonths(4);
+        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, environment);
+
+        var familiehendelseFør = ikrafttredelse1.minusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseFør)).isNull();
+
+        var familiehendelseMellom = ikrafttredelse1.plusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseMellom)).isEqualTo(UttakCore2024.DEFAULT_IKRAFTTREDELSEDATO_1); // Overstyr regelvalgdato
+
+        var familiehendelseEtter = ikrafttredelse2.plusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseEtter)).isEqualTo(UttakCore2024.DEFAULT_IKRAFTTREDELSEDATO_2); // Overstyr regelvalgdato
+    }
+
+    @Test
+    void dagensDatoEtterFørsteIkrafttredelsesdatoMenFørAndreDEV() {
+        var environment = new MockEnvironment();
+        environment.setActiveProfiles("dev-gcp");
+
+        var ikrafttredelse1 = LocalDate.now().minusMonths(1);
+        var ikrafttredelse2 = LocalDate.now().plusMonths(1);
+        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, environment);
+
+        var familiehendelseFør = ikrafttredelse1.minusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseFør)).isNull();
+
+        var familiehendelseMellom = ikrafttredelse1.plusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseMellom)).isEqualTo(UttakCore2024.DEFAULT_IKRAFTTREDELSEDATO_1); // Overstyr regelvalgdato
+
+        var familiehendelseEtter = ikrafttredelse2.plusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseEtter)).isEqualTo(UttakCore2024.DEFAULT_IKRAFTTREDELSEDATO_2); // Overstyr regelvalgdato
+    }
+
+    @Test
+    void dagensDatoEtterIkrafttredelsesdatoeneDEV() {
+        var environment = new MockEnvironment();
+        environment.setActiveProfiles("dev-gcp");
+
+        var ikrafttredelse1 = LocalDate.now().minusMonths(4);
+        var ikrafttredelse2 = LocalDate.now().minusMonths(2);
+        var uttakcore2024 = new UttakCore2024(ikrafttredelse1, ikrafttredelse2, environment);
+
+        var familiehendelseFør = ikrafttredelse1.minusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseFør)).isNull();
+
+        var familiehendelseMellom = ikrafttredelse1.plusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseMellom)).isEqualTo(UttakCore2024.DEFAULT_IKRAFTTREDELSEDATO_1);
+
+        var familiehendelseEtter = ikrafttredelse2.plusMonths(1);
+        assertThat(uttakcore2024.utledRegelvalgsdato(familiehendelseEtter)).isEqualTo(UttakCore2024.DEFAULT_IKRAFTTREDELSEDATO_2);
     }
 }
