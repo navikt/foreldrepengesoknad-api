1ebddd94f9f9ff0a281ccb2e058e0ecca7e1317a
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2024-06-19 15:16:02 +0200

    Kontotjeneste utvides med flerbarnsdager (#2707)

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/uttak/KontoBeregningDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/uttak/KontoBeregningDto.java
@@ -3,9 +3,12 @@ package no.nav.foreldrepenger.selvbetjening.uttak;
 import java.util.List;
 
 
-public record KontoBeregningDto(List<KontoDto> kontoer, Minsteretter minsteretter) {
+public record KontoBeregningDto(List<KontoDto> kontoer, Minsteretter minsteretter, Tillegg tillegg) {
 
     public record Minsteretter(int farRundtFødsel, int toTette) {
+    }
+
+    public record Tillegg(int flerbarn, int prematur) {
     }
 
     public record KontoDto(KontoType konto, int dager) {
