109761a526d9058030c7f680b0ffc6f46f446141
johannbm <johannes.moskvil@gmail.com>
2024-10-15 18:30:55 +0200

    Slå opp arbeidsgiverNavn

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InntektsmeldingDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InntektsmeldingDto.java
@@ -8,7 +8,7 @@ import java.util.List;
 public record InntektsmeldingDto(
     int versjon, BigDecimal inntektPrMnd,
                               BigDecimal refusjonPrMnd,
-                              String arbeidsgiver,
+                              String arbeidsgiverNavn,
                               String journalpostId,
     String kontaktpersonNavn,
     String kontaktpersonNummer,
