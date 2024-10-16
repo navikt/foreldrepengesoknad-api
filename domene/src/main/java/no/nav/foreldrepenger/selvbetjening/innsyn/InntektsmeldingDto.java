f4286bceaf53b36938f777362f65c5183a4d14f9
johannbm <johannes.moskvil@gmail.com>
2024-10-16 19:23:07 +0200

    Legg til stillingsprosent

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InntektsmeldingDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InntektsmeldingDto.java
@@ -8,6 +8,7 @@ import java.util.List;
 public record InntektsmeldingDto(
     int versjon,
     boolean erAktiv,
+    BigDecimal stillingsprosent,
     BigDecimal inntektPrMnd,
                               BigDecimal refusjonPrMnd,
                               String arbeidsgiverNavn,
