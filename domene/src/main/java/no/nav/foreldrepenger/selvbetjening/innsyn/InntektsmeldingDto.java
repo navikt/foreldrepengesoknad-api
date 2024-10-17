7c5ad2abeb0482174b97fee315de4a37d3a0eb9e
johannbm <johannes.moskvil@gmail.com>
2024-10-17 12:52:47 +0200

    rename felter

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InntektsmeldingDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/InntektsmeldingDto.java
@@ -5,31 +5,13 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record InntektsmeldingDto(
-    int versjon,
-    boolean erAktiv,
-    BigDecimal stillingsprosent,
-    BigDecimal inntektPrMnd,
-                              BigDecimal refusjonPrMnd,
-                              String arbeidsgiverNavn,
-                              String journalpostId,
-    String kontaktpersonNavn,
-    String kontaktpersonNummer,
-                              LocalDateTime innsendingstidspunkt,
-                              LocalDateTime mottattTidspunkt,
-                              LocalDate startDatoPermisjon,
-                              List<NaturalYtelse> bortfalteNaturalytelser,
-                              List<Refusjon> refusjonsperioder
-) {
-    public record NaturalYtelse(
-        LocalDate fomDato,
-        LocalDate tomDato,
-        BigDecimal beloepPerMnd,
-        String type
-    ) {}
+public record InntektsmeldingDto(int versjon, boolean erAktiv, BigDecimal stillingsprosent, BigDecimal inntektPrMnd, BigDecimal refusjonPrMnd,
+                                 String arbeidsgiverNavn, String journalpostId, String kontaktpersonNavn, String kontaktpersonTelefonNummer,
+                                 LocalDateTime innsendingstidspunkt, LocalDateTime mottattTidspunkt, LocalDate startDatoPermisjon,
+                                 List<NaturalYtelse> bortfalteNaturalytelser, List<Refusjon> refusjonsperioder) {
+    public record NaturalYtelse(LocalDate fomDato, LocalDate tomDato, BigDecimal beløpPerMnd, String type) {
+    }
 
-    public record Refusjon(
-        BigDecimal refusjonsbeløpMnd,
-        LocalDate fomDato
-    ) {}
+    public record Refusjon(BigDecimal refusjonsbeløpMnd, LocalDate fomDato) {
+    }
 }
