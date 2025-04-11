926b01166844c70af8551e753cda1726a3d79f3e
espenwaaga <espen.waaga@nav.no>
2025-04-11 18:38:22 +0200

    TFP-6065: Sammenligning av arbeidsforhold er uavhengig av rekkefølge

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/PersonMedArbeidsforholdDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/PersonMedArbeidsforholdDto.java
@@ -4,6 +4,35 @@ import no.nav.foreldrepenger.common.domain.felles.Person;
 import no.nav.foreldrepenger.selvbetjening.oppslag.domain.Arbeidsforhold;
 
 import java.util.List;
+import java.util.Objects;
 
 public record PersonMedArbeidsforholdDto(Person person, List<Arbeidsforhold> arbeidsforhold) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        PersonMedArbeidsforholdDto that = (PersonMedArbeidsforholdDto) o;
+        return Objects.equals(person, that.person) && LikArbeidsforhold(that);
+    }
+
+    private boolean LikArbeidsforhold(PersonMedArbeidsforholdDto that) {
+        if (arbeidsforhold == null && that.arbeidsforhold == null) {
+            return true;
+        }
+
+        if (arbeidsforhold == null || that.arbeidsforhold == null) {
+            return false;
+        }
+
+        if (arbeidsforhold.size() != that.arbeidsforhold.size()) {
+            return false;
+        }
+
+        return arbeidsforhold.containsAll(that.arbeidsforhold);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(person, arbeidsforhold);
+    }
 }
