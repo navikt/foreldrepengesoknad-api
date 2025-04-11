4696addbd66a98b64c69f222e6fa0f3043b6db18
espenwaaga <espen.waaga@nav.no>
2025-04-11 18:00:38 +0200

    TFP-6065: Tilpasser equals metoden til Arbeidsforhold slik at eksisterende differanse i stillingsprosent ikke er diff

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/domain/Arbeidsforhold.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/domain/Arbeidsforhold.java
@@ -1,10 +1,10 @@
 package no.nav.foreldrepenger.selvbetjening.oppslag.domain;
 
-import java.time.LocalDate;
-
 import com.fasterxml.jackson.annotation.JsonAlias;
-
 import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.Objects;
 
 public record Arbeidsforhold(@NotNull String arbeidsgiverId,
                              @NotNull String arbeidsgiverIdType,
@@ -13,4 +13,28 @@ public record Arbeidsforhold(@NotNull String arbeidsgiverId,
                              @NotNull @JsonAlias("from") LocalDate fom,
                              @JsonAlias("to") LocalDate tom) {
 
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Arbeidsforhold that = (Arbeidsforhold) o;
+        return Objects.equals(fom, that.fom) &&
+                Objects.equals(tom, that.tom) &&
+                Objects.equals(arbeidsgiverId, that.arbeidsgiverId) &&
+                Objects.equals(arbeidsgiverNavn, that.arbeidsgiverNavn) &&
+                sammenlignProsentRelativTilFomOgTomSomMottakIkkeHensyntar(that) &&
+                Objects.equals(arbeidsgiverIdType, that.arbeidsgiverIdType);
+    }
+
+    private boolean sammenlignProsentRelativTilFomOgTomSomMottakIkkeHensyntar(Arbeidsforhold that) {
+        if (tom != null && LocalDate.now().isAfter(tom)) {
+            return true; // Feil i mottak hvor perioden ikke hensyntas
+        }
+
+        return Objects.equals(stillingsprosent, that.stillingsprosent);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(arbeidsgiverId, arbeidsgiverIdType, arbeidsgiverNavn, stillingsprosent, fom, tom);
+    }
 }
---
+++ domene/src/test/java/no/nav/foreldrepenger/selvbetjening/oppslag/domain/ArbeidsforholdTest.java
@@ -0,0 +1,76 @@
+package no.nav.foreldrepenger.selvbetjening.oppslag.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ArbeidsforholdTest {
+
+
+    @Test
+    void hvis_arbeidsforhold_er_gyldig_sammenlignes_stillingsprosent_er_da_ulik() {
+        var arbeidsforhold1 = new Arbeidsforhold(
+                "123456789",
+                "orgnr",
+                "NAVN",
+                100.0,
+                LocalDate.now().minusYears(2),
+                null
+        );
+        var arbeidsforhold2 = new Arbeidsforhold(
+                "123456789",
+                "orgnr",
+                "NAVN",
+                50.0,
+                LocalDate.now().minusYears(2),
+                null
+        );
+        assertThat(arbeidsforhold1).isNotEqualTo(arbeidsforhold2);
+    }
+
+    @Test
+    void hvis_arbeidsforhold_er_gyldig_sammenlignes_stillingsprosent_er_da_lik() {
+        var arbeidsforhold1 = new Arbeidsforhold(
+                "123456789",
+                "orgnr",
+                "NAVN",
+                100.0,
+                LocalDate.now().minusYears(2),
+                null
+        );
+        var arbeidsforhold2 = new Arbeidsforhold(
+                "123456789",
+                "orgnr",
+                "NAVN",
+                100.0,
+                LocalDate.now().minusYears(2),
+                null
+        );
+        assertThat(arbeidsforhold1).isEqualTo(arbeidsforhold2);
+    }
+
+
+
+    @Test
+    void utgått_AF_skal_ikke_sammenling_stillingsprosent_siden_OVERSIKT_er_mer_riktig() {
+        var arbeidsforhold1 = new Arbeidsforhold(
+                "123456789",
+                "orgnr",
+                "NAVN",
+                100.0,
+                LocalDate.now().minusYears(2),
+                LocalDate.now().minusMonths(5)
+        );
+        var arbeidsforhold2 = new Arbeidsforhold(
+                "123456789",
+                "orgnr",
+                "NAVN",
+                null,
+                LocalDate.now().minusYears(2),
+                LocalDate.now().minusMonths(5)
+        );
+        assertThat(arbeidsforhold1).isEqualTo(arbeidsforhold2);
+    }
+}
