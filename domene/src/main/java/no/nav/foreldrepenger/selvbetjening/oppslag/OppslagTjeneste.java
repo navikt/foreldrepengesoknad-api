faec07e0cac4645ad5378494ac8a3e45f3fd2fa8
Espen Waaga <36693523+espenwaaga@users.noreply.github.com>
2025-04-11 12:38:55 +0200

    TFP-6065: Kaller fpoversikt for å hente informasjon om søker og samme… (#2782)
    * TFP-6065: Kaller fpoversikt for å hente informasjon om søker og sammenligner med originalt kall mot mottak for avvik
    
    * Rydder i sammenligning og gjør sammenligning mot nytt samlet api hvor både personinfo og arbeidsforhold returner slik at vi slipper å kalle to ganger.

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/OppslagTjeneste.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/OppslagTjeneste.java
@@ -1,15 +1,14 @@
 package no.nav.foreldrepenger.selvbetjening.oppslag;
 
-import static no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL;
-import static no.nav.foreldrepenger.selvbetjening.oppslag.mapper.PersonMapper.tilPersonFrontend;
-
+import no.nav.foreldrepenger.selvbetjening.oppslag.domain.PersonFrontend;
+import no.nav.foreldrepenger.selvbetjening.oppslag.domain.Søkerinfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import no.nav.foreldrepenger.selvbetjening.oppslag.domain.PersonFrontend;
-import no.nav.foreldrepenger.selvbetjening.oppslag.domain.Søkerinfo;
+import static no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL;
+import static no.nav.foreldrepenger.selvbetjening.oppslag.mapper.PersonMapper.tilPersonFrontend;
 
 @Service
 public class OppslagTjeneste implements Oppslag {
@@ -24,7 +23,9 @@ public class OppslagTjeneste implements Oppslag {
 
     @Override
     public PersonFrontend hentPerson() {
-        return tilPersonFrontend(oppslag.hentPerson());
+        var person = oppslag.hentPerson();
+        oppslag.sammenlignMedNyttEndepunktIOversikt(person);
+        return tilPersonFrontend(person);
     }
 
     @Override
@@ -34,7 +35,10 @@ public class OppslagTjeneste implements Oppslag {
 
     private Søkerinfo søkerinfo() {
         LOG.info("Henter søkerinfo");
-        var info = new Søkerinfo(tilPersonFrontend(oppslag.hentPerson()), oppslag.hentArbeidsForhold());
+        var personDto = oppslag.hentPerson();
+        var arbeidsforhold = oppslag.hentArbeidsForhold();
+        oppslag.sammenlignResultatNyttKallIOversikt(personDto, arbeidsforhold);
+        var info = new Søkerinfo(tilPersonFrontend(personDto), arbeidsforhold);
         LOG.info("Hentet søkerinfo for med {} arbeidsforhold OK", info.arbeidsforhold().size());
         LOG.trace(CONFIDENTIAL, "Hentet søkerinfo {}", info);
         return info;
