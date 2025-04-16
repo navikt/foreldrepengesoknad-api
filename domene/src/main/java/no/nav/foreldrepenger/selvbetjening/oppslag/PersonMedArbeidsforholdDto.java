d854e8e623008a579f441342e00c10d5e5d3298f
espenwaaga <espen.waaga@nav.no>
2025-04-16 12:19:23 +0200

    TFP-6065: Sammenligning av barn-liste er uavhengig av rekkefølge + fiks av sammenligning for arbeidsforhold-listen

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/PersonMedArbeidsforholdDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/oppslag/PersonMedArbeidsforholdDto.java
@@ -3,6 +3,7 @@ package no.nav.foreldrepenger.selvbetjening.oppslag;
 import no.nav.foreldrepenger.common.domain.felles.Person;
 import no.nav.foreldrepenger.selvbetjening.oppslag.domain.Arbeidsforhold;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -12,23 +13,17 @@ public record PersonMedArbeidsforholdDto(Person person, List<Arbeidsforhold> arb
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         PersonMedArbeidsforholdDto that = (PersonMedArbeidsforholdDto) o;
-        return Objects.equals(person, that.person) && LikArbeidsforhold(that);
+        return Objects.equals(person, that.person) && likArbeidsforhold(that);
     }
 
-    private boolean LikArbeidsforhold(PersonMedArbeidsforholdDto that) {
-        if (arbeidsforhold == null && that.arbeidsforhold == null) {
-            return true;
-        }
+    private boolean likArbeidsforhold(PersonMedArbeidsforholdDto that) {
+        if (Objects.equals(arbeidsforhold, that.arbeidsforhold)) return true;
+        if (arbeidsforhold == null || that.arbeidsforhold == null) return false;
+        if (arbeidsforhold.size() != that.arbeidsforhold.size()) return false;
 
-        if (arbeidsforhold == null || that.arbeidsforhold == null) {
-            return false;
-        }
-
-        if (arbeidsforhold.size() != that.arbeidsforhold.size()) {
-            return false;
-        }
-
-        return arbeidsforhold.containsAll(that.arbeidsforhold);
+        return arbeidsforhold.stream().allMatch(b ->
+                Collections.frequency(arbeidsforhold, b) == Collections.frequency(that.arbeidsforhold, b)
+        );
     }
 
     @Override
