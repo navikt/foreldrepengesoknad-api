90bd8b31952f3599673467f474373ffc55a53551
espenwaaga <espen.waaga@nav.no>
2025-05-05 13:31:34 +0200

    TFP-6127: Fjerner integarsjon mot safselvbetjening etter sammenligning

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/dokument/DokumentController.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/dokument/DokumentController.java
@@ -4,92 +4,33 @@ import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.common.domain.Saksnummer;
 import no.nav.foreldrepenger.selvbetjening.http.ProtectedRestController;
-import no.nav.foreldrepenger.selvbetjening.http.TokenUtil;
 import no.nav.foreldrepenger.selvbetjening.innsyn.Innsyn;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.HttpClientErrorException;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 @ProtectedRestController("/rest/dokument")
 public class DokumentController {
-    private static final Logger LOG = LoggerFactory.getLogger(DokumentController.class);
-    private final DokumentTjeneste dokumentTjeneste;
+
     private final Innsyn innsyn;
-    private final TokenUtil tokenUtil;
 
     @Autowired
-    public DokumentController(DokumentTjeneste dokumentTjeneste, Innsyn innsyn, TokenUtil tokenUtil) {
-        this.dokumentTjeneste = dokumentTjeneste;
+    public DokumentController(Innsyn innsyn) {
         this.innsyn = innsyn;
-        this.tokenUtil = tokenUtil;
     }
 
     @GetMapping(value = "/hent-dokument/{journalpostId}/{dokumentId}")
-    public ResponseEntity<byte[]> hentDokumentV2(@Valid @PathVariable("journalpostId") JournalpostId journalpostId,
-                                                 @Valid @PathVariable("dokumentId") DokumentInfoId dokumentId) {
-        try {
-            var response = dokumentTjeneste.hentDokument(journalpostId, dokumentId);
-            sammenlignDokument(response.getBody(), journalpostId, dokumentId);
-            LOG.info("Hentet dokument med journalpostid {} og dokumentid {}", journalpostId, dokumentId);
-            return response;
-        } catch (HttpClientErrorException.NotFound | HttpClientErrorException.Forbidden e) {
-            LOG.warn("Klarte ikke hente dokument med journalpostid {} og dokumentid {} pga {}", journalpostId.value(), dokumentId.value(),
-                e.getStatusCode(), e);
-            return ResponseEntity.notFound().build();
-        }
+    public byte[] hentDokumentV2(@Valid @PathVariable("journalpostId") JournalpostId journalpostId,
+                                 @Valid @PathVariable("dokumentId") DokumentInfoId dokumentId) {
+            return innsyn.hentDokument(journalpostId, dokumentId);
     }
 
-    private void sammenlignDokument(byte[] gammel, JournalpostId journalpostId, DokumentInfoId dokumentId) {
-        try {
-            var ny = innsyn.hentDokument(journalpostId, dokumentId);
-            if (Arrays.equals(gammel, ny)) {
-                LOG.info("DOKUMENT: Dokumentene er like");
-            } else {
-                LOG.info("DOKUMENT: Dokumentene er ulike!");
-            }
-        } catch (Exception e) {
-            LOG.info("DOKUMENT: Sammenligning av dokument feilet", e);
-        }
-    }
 
     @GetMapping(value = "/alle")
     public List<DokumentDto> hentDokumentoversikten(@RequestParam @Valid @NotNull Saksnummer saksnummer) {
-        var dokumenter = dokumentTjeneste.alle(tokenUtil.innloggetBrukerOrElseThrowException(), saksnummer);
-        LOG.info("Hentet {} dokumenter på sak {}", dokumenter.size(), saksnummer.value());
-        sammenlign(dokumenter, saksnummer);
-        return dokumenter;
-    }
-
-    private void sammenlign(List<DokumentDto> gammel, Saksnummer saksnummer) {
-        try {
-            var ny = innsyn.dokumenter(saksnummer);
-            if (erDokumentenLike(gammel, ny)) {
-                LOG.info("DOKUMENTENE: Dokumentene er like");
-            } else {
-                LOG.info("DOKUMENTENE: Dokumentene er ulike! Gammel {}, ny {}", gammel, ny);
-            }
-        } catch (Exception e) {
-            LOG.info("DOKUMENTENE: Sammenligning av dokumentene feilet", e);
-        }
-    }
-
-    private boolean erDokumentenLike(List<DokumentDto> gammel, List<DokumentDto> ny) {
-        if (Objects.equals(gammel, ny)) return true;
-        if (gammel == null || ny == null) return false;
-        if (gammel.size() != ny.size()) return false;
-
-        return gammel.stream().allMatch(b ->
-                Collections.frequency(gammel, b) == Collections.frequency(ny, b)
-        );
+        return innsyn.dokumenter(saksnummer);
     }
 }
--- domene/src/test/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynControllerTest.java
+++ domene/src/test/java/no/nav/foreldrepenger/selvbetjening/innsyn/InnsynControllerTest.java
@@ -1,22 +1,13 @@
 package no.nav.foreldrepenger.selvbetjening.innsyn;
 
 import no.nav.foreldrepenger.common.domain.Fødselsnummer;
-import no.nav.foreldrepenger.common.innsyn.BrukerRolle;
-import no.nav.foreldrepenger.common.innsyn.FpSak;
-import no.nav.foreldrepenger.common.innsyn.RettighetType;
-import no.nav.foreldrepenger.common.innsyn.Saker;
-import no.nav.foreldrepenger.common.innsyn.Saksnummer;
 import no.nav.foreldrepenger.selvbetjening.http.TokenUtil;
-import no.nav.foreldrepenger.selvbetjening.innsyn.dokument.DokumentDto;
-import no.nav.foreldrepenger.selvbetjening.innsyn.dokument.DokumentTjeneste;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.env.MockEnvironment;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
@@ -25,10 +16,8 @@ import static org.mockito.Mockito.when;
 
 class InnsynControllerTest {
 
-    private static final String FAKE_SAKSNUMMER = "123456789";
     private static final Fødselsnummer DUMMY_FNR = new Fødselsnummer("0000");
     private Innsyn innsyn = mock(Innsyn.class);
-    private DokumentTjeneste dokumentTjeneste = mock(DokumentTjeneste.class);
     private TokenUtil tokenUtil = mock(TokenUtil.class);
 
     @BeforeEach
@@ -36,82 +25,12 @@ class InnsynControllerTest {
         when(tokenUtil.innloggetBrukerOrElseThrowException()).thenReturn(DUMMY_FNR);
     }
 
-    @Test
-    void sakErOppdatertHvisOppdateringstidspunktErEtterMottattidspunktetTilJournalposten() {
-        var innsynController = new InnsynController(innsyn, dokumentTjeneste, tokenUtil, null);
-
-        var arkivertSøknadOmForeldrepenger = arkivertSøknadOmForeldrepenger(LocalDateTime.now().minusHours(1), FAKE_SAKSNUMMER);
-        var arkiverteDokumenter = List.of(arkivertSøknadOmForeldrepenger);
-        when(dokumentTjeneste.alle(any())).thenReturn(arkiverteDokumenter);
-        var saker = new Saker(Set.of(fpsak(FAKE_SAKSNUMMER, LocalDateTime.now())), Set.of(), Set.of());
-        when(innsyn.hentSaker()).thenReturn(saker);
-
-        assertThat(innsynController.erSakOppdatert()).isTrue();
-    }
-
-    @Test
-    void sakErIkkeOppdatertHvisOppdatertTidspunktetErFørInnsendingstidspunkt() {
-        var innsynController = new InnsynController(innsyn, dokumentTjeneste, tokenUtil, null);
-        var søknadOmForeldrepenger1 = arkivertSøknadOmForeldrepenger(LocalDateTime.now().minusHours(1), FAKE_SAKSNUMMER);
-        var søknadOmForeldrepenger2 = arkivertSøknadOmForeldrepenger(LocalDateTime.now(), FAKE_SAKSNUMMER);
-        var arkiverteDokumenter = List.of(søknadOmForeldrepenger1, søknadOmForeldrepenger2);
-
-        when(dokumentTjeneste.alle(any())).thenReturn(arkiverteDokumenter);
-
-        var saker = new Saker(Set.of(fpsak(FAKE_SAKSNUMMER, LocalDateTime.now().minusMinutes(30))), Set.of(), Set.of());
-        when(innsyn.hentSaker()).thenReturn(saker);
-
-        assertThat(innsynController.erSakOppdatert()).isFalse();
-    }
-
-    @Test
-    void ingenSakerIFpoversiktMenArkivertSøknadSkalReturnereFalse() {
-        var innsynController = new InnsynController(innsyn, dokumentTjeneste, tokenUtil, null);
-
-        var søknadOmForeldrepenger = arkivertSøknadOmForeldrepenger(LocalDateTime.now(), FAKE_SAKSNUMMER);
-        var arkiverteDokumenter = List.of(søknadOmForeldrepenger);
-        when(dokumentTjeneste.alle(any())).thenReturn(arkiverteDokumenter);
-
-        var saker = new Saker(Set.of(), Set.of(), Set.of());
-        when(innsyn.hentSaker()).thenReturn(saker);
-
-        assertThat(innsynController.erSakOppdatert()).isFalse();
-    }
-
-    @Test
-    void ingenSakerMedLiktSaksnummerIFpoversiktMenArkivertSøknadSkalReturnereFalse() {
-        var innsynController = new InnsynController(innsyn, dokumentTjeneste, tokenUtil, null);
-
-        var søknadOmForeldrepenger = arkivertSøknadOmForeldrepenger(LocalDateTime.now(), FAKE_SAKSNUMMER);
-        var arkiverteDokumenter = List.of(søknadOmForeldrepenger);
-        when(dokumentTjeneste.alle(any())).thenReturn(arkiverteDokumenter);
-
-        var saker = new Saker(Set.of(fpsak("987654321", LocalDateTime.now().minusMinutes(30))), Set.of(), Set.of());
-        when(innsyn.hentSaker()).thenReturn(saker);
-
-        assertThat(innsynController.erSakOppdatert()).isFalse();
-    }
-
-    @Test
-    void nårSøknadIkkeHarSaksnummerAntarViAtSakenIkkeErOppdatert() {
-        var innsynController = new InnsynController(innsyn, dokumentTjeneste, tokenUtil, null);
-
-        var søknadOmForeldrepenger1 = arkivertSøknadOmForeldrepenger(LocalDateTime.now(), null);
-        var søknadOmForeldrepenger2 = arkivertSøknadOmForeldrepenger(LocalDateTime.now(), FAKE_SAKSNUMMER);
-        var arkiverteDokumenter = List.of(søknadOmForeldrepenger1, søknadOmForeldrepenger2);
-        when(dokumentTjeneste.alle(any())).thenReturn(arkiverteDokumenter);
-
-        var saker = new Saker(Set.of(fpsak("987654321", LocalDateTime.now().minusMinutes(30))), Set.of(), Set.of());
-        when(innsyn.hentSaker()).thenReturn(saker);
-
-        assertThat(innsynController.erSakOppdatert()).isFalse();
-    }
 
     @Test
     void skalAlltidKreveDokumentasjonAvMorIArbeid() {
         var environment = new MockEnvironment();
         environment.setActiveProfiles("dev-gcp");
-        var innsynController = new InnsynController(innsyn, dokumentTjeneste, tokenUtil, environment);
+        var innsynController = new InnsynController(innsyn, environment);
         when(innsyn.trengerDokumentereMorsArbeid(any())).thenReturn(false);
 
         var perioder = List.of(new MorArbeidRequestDto.Periode(LocalDate.now().minusYears(2), LocalDate.now()));
@@ -128,7 +47,7 @@ class InnsynControllerTest {
     void skalAlltidKreveDokumentasjonAvMorIArbeidIProd() {
         var environment = new MockEnvironment();
         environment.setActiveProfiles("prod-gcp");
-        var innsynController = new InnsynController(innsyn, dokumentTjeneste, tokenUtil, environment);
+        var innsynController = new InnsynController(innsyn, environment);
 
         var perioder = List.of(new MorArbeidRequestDto.Periode(LocalDate.now().minusYears(2), LocalDate.now()));
         var måDokumentereMorIArbeid = innsynController.trengerDokumentereMorsArbeid(new MorArbeidRequestDto(
@@ -138,36 +57,5 @@ class InnsynControllerTest {
                 perioder));
 
         assertThat(måDokumentereMorIArbeid).isTrue();
-    }
-
-    private static DokumentDto arkivertSøknadOmForeldrepenger(LocalDateTime mottatt, String saksnummer) {
-        return new DokumentDto(
-            "Søknad om foreldrepenger",
-            DokumentDto.Type.INNGÅENDE_DOKUMENT,
-            saksnummer,
-            "12345",
-            "123456",
-            mottatt);
-    }
-
-    private static FpSak fpsak(String saksnummer, LocalDateTime oppdateringstidspunkt) {
-        return new FpSak(
-            new Saksnummer(saksnummer),
-            false,
-            true,
-            true,
-            false,
-            false,
-            false,
-            false,
-            RettighetType.BEGGE_RETT,
-            null,
-            null,
-            null,
-            null,
-            null,
-            null,
-            oppdateringstidspunkt,
-            BrukerRolle.MOR);
     }
 }
