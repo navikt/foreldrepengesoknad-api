af038e31a3504e9668c2526b9359ac88380a7e93
espenwaaga <espen.waaga@nav.no>
2025-05-07 09:53:43 +0200

    FIX: Hent dokument må sende tilbake byte array av typen application/pdf

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/dokument/DokumentController.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/dokument/DokumentController.java
@@ -1,16 +1,18 @@
 package no.nav.foreldrepenger.selvbetjening.innsyn.dokument;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import no.nav.foreldrepenger.common.domain.Saksnummer;
 import no.nav.foreldrepenger.selvbetjening.http.ProtectedRestController;
 import no.nav.foreldrepenger.selvbetjening.innsyn.Innsyn;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @ProtectedRestController("/rest/dokument")
 public class DokumentController {
@@ -22,10 +24,10 @@ public class DokumentController {
         this.innsyn = innsyn;
     }
 
-    @GetMapping(value = "/hent-dokument/{journalpostId}/{dokumentId}")
+    @GetMapping(value = "/hent-dokument/{journalpostId}/{dokumentId}", produces = MediaType.APPLICATION_PDF_VALUE)
     public byte[] hentDokumentV2(@Valid @PathVariable("journalpostId") JournalpostId journalpostId,
                                  @Valid @PathVariable("dokumentId") DokumentInfoId dokumentId) {
-            return innsyn.hentDokument(journalpostId, dokumentId);
+        return innsyn.hentDokument(journalpostId, dokumentId);
     }
 
 
