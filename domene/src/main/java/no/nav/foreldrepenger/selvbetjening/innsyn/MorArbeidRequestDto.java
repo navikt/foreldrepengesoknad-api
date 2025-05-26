e383c563ca7dcc3feb4f754b633e4dbff5009f18
Anders Palfi <anders.palfi@nav.no>
2025-05-26 14:13:59 +0200

    TFP-5383 periodetype utsettelse eller uttak på mors arbeid requesten

--- domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/MorArbeidRequestDto.java
+++ domene/src/main/java/no/nav/foreldrepenger/selvbetjening/innsyn/MorArbeidRequestDto.java
@@ -1,18 +1,18 @@
 package no.nav.foreldrepenger.selvbetjening.innsyn;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import no.nav.foreldrepenger.common.domain.Fødselsnummer;
-
-import java.time.LocalDate;
-import java.util.List;
 
 public record MorArbeidRequestDto(@Valid @NotNull Fødselsnummer annenPartFødselsnummer,
                                   @Valid Fødselsnummer barnFødselsnummer,
                                   LocalDate familiehendelse,
                                   @Valid @Size(max = 50) List<@Valid @NotNull Periode> perioder) {
 
-    public record Periode(@NotNull LocalDate fom, @NotNull LocalDate tom) {
+    public record Periode(@NotNull LocalDate fom, @NotNull LocalDate tom, @Valid PeriodeMedAktivitetskravType periodeType) {
     }
 }
